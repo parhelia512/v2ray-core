535b526c3f6214ffbb51d637ca1fc19a9eced037
eMeab <32988354+eMeab@users.noreply.github.com>
2021-01-13 23:13:51 +0800

    Add SNI shunt support for VLESS and Trojan fallbacks

--- infra/conf/v4/trojan.go
+++ infra/conf/v4/trojan.go
@@ -75,6 +75,7 @@ func (c *TrojanClientConfig) Build() (proto.Message, error) {
 
 // TrojanInboundFallback is fallback configuration
 type TrojanInboundFallback struct {
+	Name string          `json:"name"`
 	Alpn string          `json:"alpn"`
 	Path string          `json:"path"`
 	Type string          `json:"type"`
@@ -125,6 +126,7 @@ func (c *TrojanServerConfig) Build() (proto.Message, error) {
 			_ = json.Unmarshal(fb.Dest, &s)
 		}
 		config.Fallbacks = append(config.Fallbacks, &trojan.Fallback{
+			Name: fb.Name,
 			Alpn: fb.Alpn,
 			Path: fb.Path,
 			Type: fb.Type,
