391aa3b0322cc119ec8afefd099acefefbcfc113
monsoon <yjh1021317464@gmail.com>
2021-02-28 00:10:26 +0800

    Add http+socks mixed inbound protocol

--- main/distro/all/all.go
+++ main/distro/all/all.go
@@ -40,6 +40,7 @@ import (
 	_ "github.com/v2fly/v2ray-core/v5/proxy/dokodemo"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/freedom"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/http"
+	_ "github.com/v2fly/v2ray-core/v5/proxy/mixed"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/shadowsocks"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/shadowsocks_2022"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/socks"
@@ -115,6 +116,7 @@ import (
 
 	// Simplified config
 	_ "github.com/v2fly/v2ray-core/v5/proxy/http/simplified"
+	_ "github.com/v2fly/v2ray-core/v5/proxy/mixed/simplified"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/shadowsocks/simplified"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/socks/simplified"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/trojan/simplified"
