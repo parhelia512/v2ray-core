3aa5e43b033547824249c885d0c4b60fcca44570
Shelikhoo <xiaokangwang@outlook.com>
2021-06-19 11:21:35 +0100

    add support for tagged observer

--- app/observatory/command/command.go
+++ app/observatory/command/command.go
@@ -3,14 +3,18 @@
 
 package command
 
+//go:generate go run github.com/v2fly/v2ray-core/v4/common/errors/errorgen
+
 import (
 	"context"
 
+	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc"
 
 	core "github.com/v2fly/v2ray-core/v4"
 	"github.com/v2fly/v2ray-core/v4/app/observatory"
 	"github.com/v2fly/v2ray-core/v4/common"
+	"github.com/v2fly/v2ray-core/v4/features"
 	"github.com/v2fly/v2ray-core/v4/features/extension"
 )
 
@@ -22,11 +26,21 @@ type service struct {
 }
 
 func (s *service) GetOutboundStatus(ctx context.Context, request *GetOutboundStatusRequest) (*GetOutboundStatusResponse, error) {
-	resp, err := s.observatory.GetObservation(ctx)
-	if err != nil {
-		return nil, err
+	var result proto.Message
+	if request.Tag == "" {
+		observeResult, err := s.observatory.GetObservation(ctx)
+		if err != nil {
+			return nil, newError("cannot get observation").Base(err)
+		}
+		result = observeResult
+	} else {
+		observeResult, err := common.Must2(s.observatory.(features.TaggedFeatures).GetFeaturesByTag(request.Tag)).(extension.Observatory).GetObservation(ctx)
+		if err != nil {
+			return nil, newError("cannot get observation").Base(err)
+		}
+		result = observeResult
 	}
-	retdata := resp.(*observatory.ObservationResult)
+	retdata := result.(*observatory.ObservationResult)
 	return &GetOutboundStatusResponse{
 		Status: retdata,
 	}, nil
