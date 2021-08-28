190ba745e3d8d0f0d87b4edfc89ce7292d6f5fc1
世界 <sekai@neko.services>
2021-08-28 17:10:37 +0800

    Fix endpoint independent mapping for DialUDP

--- transport/internet/udp/dispatcher_split.go
+++ transport/internet/udp/dispatcher_split.go
@@ -6,7 +6,6 @@ import (
 	"sync"
 	"time"
 
-	"github.com/v2fly/v2ray-core/v5/common"
 	"github.com/v2fly/v2ray-core/v5/common/buf"
 	"github.com/v2fly/v2ray-core/v5/common/net"
 	"github.com/v2fly/v2ray-core/v5/common/protocol/udp"
@@ -21,13 +20,14 @@ type ResponseCallback func(ctx context.Context, packet *udp.Packet)
 
 type connEntry struct {
 	link   *transport.Link
+	ctx    context.Context
 	timer  signal.ActivityUpdater
 	cancel context.CancelFunc
 }
 
 type Dispatcher struct {
 	sync.RWMutex
-	conns      map[net.Destination]*connEntry
+	conn       *connEntry
 	dispatcher routing.Dispatcher
 	callback   ResponseCallback
 }
@@ -38,19 +38,8 @@ func (v *Dispatcher) Close() error {
 
 func NewSplitDispatcher(dispatcher routing.Dispatcher, callback ResponseCallback) DispatcherI {
 	return &Dispatcher{
-		conns:      make(map[net.Destination]*connEntry),
 		dispatcher: dispatcher,
 		callback:   callback,
-	}
-}
-
-func (v *Dispatcher) RemoveRay(dest net.Destination) {
-	v.Lock()
-	defer v.Unlock()
-	if conn, found := v.conns[dest]; found {
-		common.Close(conn.link.Reader)
-		common.Close(conn.link.Writer)
-		delete(v.conns, dest)
 	}
 }
 
@@ -58,25 +47,27 @@ func (v *Dispatcher) getInboundRay(ctx context.Context, dest net.Destination) *c
 	v.Lock()
 	defer v.Unlock()
 
-	if entry, found := v.conns[dest]; found {
-		return entry
+	if v.conn != nil {
+		select {
+		case <-v.conn.ctx.Done():
+			v.conn = nil
+		default:
+			return v.conn
+		}
 	}
 
 	newError("establishing new connection for ", dest).WriteToLog()
 
 	ctx, cancel := context.WithCancel(ctx)
-	removeRay := func() {
-		cancel()
-		v.RemoveRay(dest)
-	}
-	timer := signal.CancelAfterInactivity(ctx, removeRay, time.Second*300)
+	timer := signal.CancelAfterInactivity(ctx, cancel, time.Second*300)
 	link, _ := v.dispatcher.Dispatch(ctx, dest)
 	entry := &connEntry{
 		link:   link,
+		ctx:    ctx,
 		timer:  timer,
-		cancel: removeRay,
+		cancel: cancel,
 	}
-	v.conns[dest] = entry
+	v.conn = entry
 	go handleInput(ctx, entry, dest, v.callback)
 	return entry
 }
@@ -94,6 +85,7 @@ func (v *Dispatcher) Dispatch(ctx context.Context, destination net.Destination, 
 			conn.cancel()
 			return
 		}
+		conn.timer.Update()
 	}
 }
 
@@ -112,6 +104,7 @@ func handleInput(ctx context.Context, conn *connEntry, dest net.Destination, cal
 
 		mb, err := input.ReadMultiBuffer()
 		if err != nil {
+			buf.ReleaseMulti(mb)
 			newError("failed to handle UDP input").Base(err).WriteToLog(session.ExportIDToError(ctx))
 			return
 		}
@@ -129,6 +122,7 @@ func handleInput(ctx context.Context, conn *connEntry, dest net.Destination, cal
 }
 
 type dispatcherConn struct {
+	ctx        context.Context
 	dispatcher *Dispatcher
 	cache      chan *udp.Packet
 	done       *done.Instance
@@ -136,6 +130,7 @@ type dispatcherConn struct {
 
 func DialDispatcher(ctx context.Context, dispatcher routing.Dispatcher) (net.PacketConn, error) {
 	c := &dispatcherConn{
+		ctx:   ctx,
 		cache: make(chan *udp.Packet, 16),
 		done:  done.New(),
 	}
@@ -176,8 +171,9 @@ func (c *dispatcherConn) WriteTo(p []byte, addr net.Addr) (int, error) {
 	n := copy(raw, p)
 	buffer.Resize(0, int32(n))
 
-	ctx := context.Background()
-	c.dispatcher.Dispatch(ctx, net.DestinationFromAddr(addr), buffer)
+	dest := net.DestinationFromAddr(addr)
+	buffer.Endpoint = &dest
+	c.dispatcher.Dispatch(c.ctx, dest, buffer)
 	return n, nil
 }
 
