ad17ffa066246edfa3a52c78607c67ac4e99fc02
世界 <i@sekai.icu>
2021-09-14 12:58:04 +0800

    Add routeOnly sniffing option

--- common/session/session.go
+++ common/session/session.go
@@ -48,7 +48,8 @@ type Inbound struct {
 // Outbound is the metadata of an outbound connection.
 type Outbound struct {
 	// Target address of the outbound connection.
-	Target net.Destination
+	Target      net.Destination
+	RouteTarget net.Destination
 	// Gateway address
 	Gateway net.Address
 	// Domain resolver to use when dialing
@@ -60,6 +61,7 @@ type SniffingRequest struct {
 	OverrideDestinationForProtocol []string
 	Enabled                        bool
 	MetadataOnly                   bool
+	RouteOnly                      bool
 }
 
 // Content is the metadata of the connection content.
