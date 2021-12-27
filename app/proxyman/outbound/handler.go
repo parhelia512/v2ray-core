2e42c832fb21cbfcd1c34c7495a1e037c6e93ada
Shelikhoo <xiaokangwang@outlook.com>
2021-12-27 16:21:13 +0000

    add packet addr support in outbound handler

--- app/proxyman/outbound/handler.go
+++ app/proxyman/outbound/handler.go
@@ -8,6 +8,7 @@ import (
 	"github.com/v2fly/v2ray-core/v4/common"
 	"github.com/v2fly/v2ray-core/v4/common/mux"
 	"github.com/v2fly/v2ray-core/v4/common/net"
+	"github.com/v2fly/v2ray-core/v4/common/net/packetaddr"
 	"github.com/v2fly/v2ray-core/v4/common/session"
 	"github.com/v2fly/v2ray-core/v4/features/outbound"
 	"github.com/v2fly/v2ray-core/v4/features/policy"
@@ -205,6 +206,15 @@ func (h *Handler) Dial(ctx context.Context, dest net.Destination) (internet.Conn
 		tag := h.senderSettings.ProxySettings.Tag
 		newError("transport layer proxying to ", tag, " for dest ", dest).AtDebug().WriteToLog(session.ExportIDToError(ctx))
 		ctx = session.SetTransportLayerProxyTagToContext(ctx, tag)
+	}
+
+	if isStream, err := packetaddr.GetDestinationSubsetOf(dest); err == nil {
+		packetConn, err := internet.ListenSystemPacket(ctx, nil, h.streamSettings.SocketSettings)
+		if err != nil {
+			return nil, newError("unable to listen socket").Base(err)
+		}
+		conn := packetaddr.ToPacketAddrConnWrapper(packetConn, isStream)
+		return h.getStatCouterConnection(conn), nil
 	}
 
 	conn, err := internet.Dial(ctx, dest, h.streamSettings)
