4cac6b10e8319df0638652059537adeabf15b150
yuhan6665 <1588741+yuhan6665@users.noreply.github.com>
2022-01-01 11:49:14 -0500

    DialSystem for Quic
    DialSystem() is needed in case of Android client,
    where the raw conn is protected for vpn service

--- transport/internet/quic/dialer.go
+++ transport/internet/quic/dialer.go
@@ -120,7 +120,7 @@ func (s *clientConnections) cleanConnections() error {
 	return nil
 }
 
-func (s *clientConnections) openConnection(destAddr net.Addr, streamSettings *internet.MemoryStreamConfig) (internet.Connection, error) {
+func (s *clientConnections) openConnection(ctx context.Context, destAddr net.Addr, streamSettings *internet.MemoryStreamConfig) (internet.Connection, error) {
 	s.access.Lock()
 	defer s.access.Unlock()
 
@@ -146,12 +146,9 @@ func (s *clientConnections) openConnection(destAddr net.Addr, streamSettings *in
 
 	newError("dialing QUIC to ", dest).WriteToLog()
 
-	rawConn, err := internet.ListenSystemPacket(context.Background(), &net.UDPAddr{
-		IP:   []byte{0, 0, 0, 0},
-		Port: 0,
-	}, streamSettings.SocketSettings)
+	rawConn, err := internet.DialSystem(ctx, dest, streamSettings.SocketSettings)
 	if err != nil {
-		return nil, err
+		return nil, newError("failed to dial to dest: ", err).AtWarning().Base(err)
 	}
 
 	quicConfig := &quic.Config{
@@ -160,7 +157,18 @@ func (s *clientConnections) openConnection(destAddr net.Addr, streamSettings *in
 		KeepAlivePeriod:      time.Second * 15,
 	}
 
-	sysConn, err := wrapSysConn(rawConn.(*net.UDPConn), streamSettings.ProtocolSettings.(*Config))
+	var udpConn *net.UDPConn
+	switch conn := rawConn.(type) {
+	case *net.UDPConn:
+		udpConn = conn
+	case *internet.PacketConnWrapper:
+		udpConn = conn.Conn.(*net.UDPConn)
+	default:
+		rawConn.Close()
+		return nil, newError("QUIC with sockopt is unsupported").AtWarning()
+	}
+
+	sysConn, err := wrapSysConn(udpConn, streamSettings.ProtocolSettings.(*Config))
 	if err != nil {
 		rawConn.Close()
 		return nil, err
@@ -220,7 +228,7 @@ func Dial(ctx context.Context, dest net.Destination, streamSettings *internet.Me
 		destAddr = addr
 	}
 
-	return client.openConnection(destAddr, streamSettings)
+	return client.openConnection(ctx, destAddr, streamSettings)
 }
 
 func init() {
