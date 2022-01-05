8bdcd0ee820f6e3d34ea566b25882a56f14b46e7
世界 <i@sekai.icu>
2022-01-05 19:51:43 +0800

    Add hook to improve observatory

---
+++ app/observatory/persist.go
@@ -0,0 +1,13 @@
+package observatory
+
+// SagerNet private
+func (o *Observer) UpdateStatus(result *OutboundStatus) {
+	o.statusLock.Lock()
+	defer o.statusLock.Unlock()
+
+	if location := o.findStatusLocationLockHolderOnly(result.OutboundTag); location != -1 {
+		o.status[location] = result
+	} else {
+		o.status = append(o.status, result)
+	}
+}
