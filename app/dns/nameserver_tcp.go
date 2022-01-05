5b59cf40d06f801d07a7cc6abc7c897586d45c7e
世界 <i@sekai.icu>
2022-01-05 19:49:09 +0800

    Hook to replace dns dialer

--- app/dns/nameserver_tcp.go
+++ app/dns/nameserver_tcp.go
@@ -70,7 +70,7 @@ func NewTCPLocalNameServer(url *url.URL) (*TCPNameServer, error) {
 	}
 
 	s.dial = func(ctx context.Context) (net.Conn, error) {
-		return internet.DialSystem(ctx, s.destination, nil)
+		return internet.DialSystemDNS(ctx, s.destination, nil)
 	}
 
 	return s, nil
