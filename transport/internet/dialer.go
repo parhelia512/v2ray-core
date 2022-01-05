04675d960f6c702e653a066741ef283ed4f94788
世界 <i@sekai.icu>
2022-01-05 19:49:09 +0800

    Hook to replace dns dialer

--- transport/internet/dialer.go
+++ transport/internet/dialer.go
@@ -98,6 +98,38 @@ func DialSystem(ctx context.Context, dest net.Destination, sockopt *SocketConfig
 	return effectiveSystemDialer.Dial(ctx, src, dest, sockopt)
 }
 
+// SagerNet: private
+func DialSystemDNS(ctx context.Context, dest net.Destination, sockopt *SocketConfig) (net.Conn, error) {
+	outbound := session.OutboundFromContext(ctx)
+
+	var src net.Address
+	if outbound != nil {
+		src = outbound.Gateway
+	}
+
+	if transportLayerOutgoingTag := session.GetTransportLayerProxyTagFromContext(ctx); transportLayerOutgoingTag != "" {
+		return DialTaggedOutbound(ctx, dest, transportLayerOutgoingTag)
+	}
+
+	originalAddr := dest.Address
+	if outbound != nil && outbound.Resolver != nil && dest.Address.Family().IsDomain() {
+		if addr := outbound.Resolver(ctx, dest.Address.Domain()); addr != nil {
+			dest.Address = addr
+		}
+	}
+
+	switch {
+	case src != nil && dest.Address != originalAddr:
+		newError("dialing to ", dest, " resolved from ", originalAddr, " via ", src).WriteToLog(session.ExportIDToError(ctx))
+	case src != nil:
+		newError("dialing to ", dest, " via ", src).WriteToLog(session.ExportIDToError(ctx))
+	case dest.Address != originalAddr:
+		newError("dialing to ", dest, " resolved from ", originalAddr).WriteToLog(session.ExportIDToError(ctx))
+	}
+
+	return effectiveSystemDNSDialer.Dial(ctx, src, dest, sockopt)
+}
+
 func DialTaggedOutbound(ctx context.Context, dest net.Destination, tag string) (net.Conn, error) {
 	if tagged.Dialer == nil {
 		return nil, newError("tagged dial not enabled")
