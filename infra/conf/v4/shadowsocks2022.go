6585ffbb5646e63359b0d191a4847928fa6a6850
世界 <i@sekai.icu>
2022-01-08 16:30:44 +0800

    Add Shadowsocks SIP003 plugin support

--- infra/conf/v4/shadowsocks2022.go
+++ infra/conf/v4/shadowsocks2022.go
@@ -8,11 +8,14 @@ import (
 )
 
 type Shadowsocks2022Config struct {
-	Method  string             `json:"method"`
-	PSK     []byte             `json:"psk"`
-	IPSK    [][]byte           `json:"iPSK"`
-	Address *cfgcommon.Address `json:"address"`
-	Port    uint16             `json:"port"`
+	Method     string                `json:"method"`
+	PSK        []byte                `json:"psk"`
+	IPSK       [][]byte              `json:"iPSK"`
+	Address    *cfgcommon.Address    `json:"address"`
+	Port       uint16                `json:"port"`
+	Plugin     string                `json:"plugin"`
+	PluginOpts string                `json:"pluginOpts"`
+	PluginArgs *cfgcommon.StringList `json:"pluginArgs"`
 }
 
 func (c *Shadowsocks2022Config) Build() (proto.Message, error) {
@@ -22,5 +25,10 @@ func (c *Shadowsocks2022Config) Build() (proto.Message, error) {
 	config.Ipsk = c.IPSK
 	config.Address = c.Address.Build()
 	config.Port = uint32(c.Port)
+	config.Plugin = c.Plugin
+	config.PluginOpts = c.PluginOpts
+	if c.PluginArgs != nil && len(*c.PluginArgs) > 0 {
+		config.PluginArgs = *c.PluginArgs
+	}
 	return config, nil
 }
