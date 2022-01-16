9f3fa87e1e0785e19e125b6e9e9595150e287b61
Shelikhoo <xiaokangwang@outlook.com>
2022-01-16 23:06:02 +0000

    add jsonConf support for bindToDevice, bufSize

--- infra/conf/transport_internet.go
+++ infra/conf/transport_internet.go
@@ -392,6 +392,10 @@ type SocketConfig struct {
 	AcceptProxyProtocol  bool   `json:"acceptProxyProtocol"`
 	TCPKeepAliveInterval int32  `json:"tcpKeepAliveInterval"`
 	TCPKeepAliveIdle     int32  `json:"tcpKeepAliveIdle"`
+	BindToDevice         string `json:"bindToDevice"`
+	RxBufSize            uint64 `json:"rxBufSize"`
+	TxBufSize            uint64 `json:"txBufSize"`
+	ForceBufSize         bool   `json:"forceBufSize"`
 }
 
 // Build implements Buildable.
@@ -428,6 +432,10 @@ func (c *SocketConfig) Build() (*internet.SocketConfig, error) {
 		AcceptProxyProtocol:  c.AcceptProxyProtocol,
 		TcpKeepAliveInterval: c.TCPKeepAliveInterval,
 		TcpKeepAliveIdle:     c.TCPKeepAliveIdle,
+		RxBufSize:            int64(c.RxBufSize),
+		TxBufSize:            int64(c.TxBufSize),
+		ForceBufSize:         c.ForceBufSize,
+		BindToDevice:         c.BindToDevice,
 	}, nil
 }
 
