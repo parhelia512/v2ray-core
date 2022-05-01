xiaokangwang/v2ray-core-1

incorporate changes in router implementation

app/router/strategy_leastload.go
@@ -1,25 +1,29 @@
 package router
 
 import (
-	"fmt"
-	"github.com/v2fly/v2ray-core/v4/app/observatory/burst"
+	"context"
+	core "github.com/v2fly/v2ray-core/v4"
+	"github.com/v2fly/v2ray-core/v4/app/observatory"
+	"github.com/v2fly/v2ray-core/v4/common"
 	"math"
 	"sort"
-	"strings"
 	"time"
 
 	"github.com/v2fly/v2ray-core/v4/common/dice"
-	"github.com/v2fly/v2ray-core/v4/features/routing"
 )
 
 // LeastLoadStrategy represents a least load balancing strategy
 type LeastLoadStrategy struct {
 	settings *StrategyLeastLoadConfig
 	costs    *WeightManager
+
+	observer *observatory.Observer
+
+	ctx context.Context
 }
 
 // NewLeastLoadStrategy creates a new LeastLoadStrategy with settings
-func NewLeastLoadStrategy(settings *StrategyLeastLoadConfig, dispatcher routing.Dispatcher) *LeastLoadStrategy {
+func NewLeastLoadStrategy(settings *StrategyLeastLoadConfig) *LeastLoadStrategy {
 	return &LeastLoadStrategy{
 		settings: settings,
 		costs: NewWeightManager(
@@ -41,30 +45,18 @@ type node struct {
 	RTTAverage       time.Duration
 	RTTDeviation     time.Duration
 	RTTDeviationCost time.Duration
-
-	applied time.Duration
 }
 
-// GetInformation implements the routing.BalancingStrategy.
-func (s *LeastLoadStrategy) GetInformation(tags []string) *routing.StrategyInfo {
-	qualified, others := s.getNodes(tags, time.Duration(s.settings.MaxRTT))
-	selects := s.selectLeastLoad(qualified)
-	// append qualified but not selected outbounds to others
-	others = append(others, qualified[len(selects):]...)
-	leastloadSort(others)
-	titles, sl := s.getNodesInfo(selects)
-	_, ot := s.getNodesInfo(others)
-	return &routing.StrategyInfo{
-		Settings:    s.getSettings(),
-		ValueTitles: titles,
-		Selects:     sl,
-		Others:      ot,
-	}
+func (l *LeastLoadStrategy) InjectContext(ctx context.Context) {
+	l.ctx = ctx
+
+	common.Must(core.RequireFeatures(ctx, func(observerInstance *observatory.Observer) {
+		l.observer = observerInstance
+	}))
 }
 
-// SelectAndPick implements the routing.BalancingStrategy.
-func (s *LeastLoadStrategy) SelectAndPick(candidates []string) string {
-	qualified, _ := s.getNodes(candidates, time.Duration(s.settings.MaxRTT))
+func (s *LeastLoadStrategy) PickOutbound(candidates []string) string {
+	qualified := s.getNodes(candidates, time.Duration(s.settings.MaxRTT))
 	selects := s.selectLeastLoad(qualified)
 	count := len(selects)
 	if count == 0 {
@@ -74,19 +66,9 @@ func (s *LeastLoadStrategy) SelectAndPick(candidates []string) string {
 	return selects[dice.Roll(count)].Tag
 }
 
-// Pick implements the routing.BalancingStrategy.
-func (s *LeastLoadStrategy) Pick(candidates []string) string {
-	count := len(candidates)
-	if count == 0 {
-		// goes to fallbackTag
-		return ""
-	}
-	return candidates[dice.Roll(count)]
-}
-
 // selectLeastLoad selects nodes according to Baselines and Expected Count.
 //
-// The strategy always improves network response speed, not matter which mode below is configurated.
+// The strategy always improves network response speed, not matter which mode below is configured.
 // But they can still have different priorities.
 //
 // 1. Bandwidth priority: no Baseline + Expected Count > 0.: selects `Expected Count` of nodes.
@@ -123,7 +105,7 @@ func (s *LeastLoadStrategy) selectLeastLoad(nodes []*node) []*node {
 	for _, b := range s.settings.Baselines {
 		baseline := time.Duration(b)
 		for i := 0; i < availableCount; i++ {
-			if nodes[i].applied > baseline {
+			if nodes[i].RTTDeviationCost > baseline {
 				break
 			}
 			count = i + 1
@@ -140,163 +122,49 @@ func (s *LeastLoadStrategy) selectLeastLoad(nodes []*node) []*node {
 	return nodes[:count]
 }
 
-func (s *LeastLoadStrategy) getNodes(candidates []string, maxRTT time.Duration) ([]*node, []*node) {
-	s.access.Lock()
-	defer s.access.Unlock()
-	results := s.Results
-	qualified := make([]*node, 0)
-	unqualified := make([]*node, 0)
-	failed := make([]*node, 0)
-	untested := make([]*node, 0)
-	others := make([]*node, 0)
-	for _, tag := range candidates {
-		r, ok := results[tag]
-		if !ok {
-			untested = append(untested, &node{
-				Tag:              tag,
-				RTTDeviationCost: 0,
-				RTTDeviation:     0,
-				RTTAverage:       0,
-				applied:          rttUntested,
-			})
-			continue
-		}
-		stats := r.Get()
-		node := &node{
-			Tag:              tag,
-			RTTDeviationCost: time.Duration(s.costs.Apply(tag, float64(stats.Deviation))),
-			RTTDeviation:     stats.Deviation,
-			RTTAverage:       stats.Average,
-			CountAll:         stats.All,
-			CountFail:        stats.Fail,
-		}
-		switch {
-		case stats.All == 0:
-			node.applied = rttUntested
-			untested = append(untested, node)
-		case maxRTT > 0 && stats.Average > maxRTT:
-			node.applied = rttUnqualified
-			unqualified = append(unqualified, node)
-		case float64(stats.Fail)/float64(stats.All) > float64(s.settings.Tolerance):
-			node.applied = rttFailed
-			if stats.All-stats.Fail == 0 {
-				// no good, put them after has-good nodes
-				node.RTTDeviationCost = rttFailed
-				node.RTTDeviation = rttFailed
-				node.RTTAverage = rttFailed
-			}
-			failed = append(failed, node)
-		default:
-			node.applied = node.RTTDeviationCost
-			qualified = append(qualified, node)
-		}
+func (s *LeastLoadStrategy) getNodes(candidates []string, maxRTT time.Duration) []*node {
+	observeResult, err := s.observer.GetObservation(s.ctx)
+	if err != nil {
+		newError("cannot get observation").Base(err)
 	}
-	if len(qualified) > 0 {
-		leastloadSort(qualified)
-		others = append(others, unqualified...)
-		others = append(others, untested...)
-		others = append(others, failed...)
-	} else {
-		qualified = untested
-		others = append(others, unqualified...)
-		others = append(others, failed...)
-	}
-	return qualified, others
-}
 
-func (s *LeastLoadStrategy) getSettings() []string {
-	settings := make([]string, 0)
-	sb := new(strings.Builder)
-	for i, b := range s.settings.Baselines {
-		if i > 0 {
-			sb.WriteByte(' ')
-		}
-		sb.WriteString(time.Duration(b).String())
-	}
-	baselines := sb.String()
-	if baselines == "" {
-		baselines = "none"
-	}
-	maxRTT := time.Duration(s.settings.MaxRTT).String()
-	if s.settings.MaxRTT == 0 {
-		maxRTT = "none"
-	}
-	settings = append(settings, fmt.Sprintf(
-		"leastload, expected: %d, baselines: %s, max rtt: %s, tolerance: %.2f",
-		s.settings.Expected,
-		baselines,
-		maxRTT,
-		s.settings.Tolerance,
-	))
-	settings = append(settings, fmt.Sprintf(
-		"health ping, interval: %s, sampling: %d, timeout: %s, destination: %s",
-		s.HealthPing.Settings.Interval,
-		s.HealthPing.Settings.SamplingCount,
-		s.HealthPing.Settings.Timeout,
-		s.HealthPing.Settings.Destination,
-	))
-	return settings
-}
+	results := observeResult.(*observatory.ObservationResult)
 
-func (s *LeastLoadStrategy) getNodesInfo(nodes []*node) ([]string, []*routing.OutboundInfo) {
-	titles := []string{"   ", "RTT STD+C    ", "RTT STD.     ", "RTT Avg.     ", "Hit  ", "Cost "}
-	hasCost := len(s.settings.Costs) > 0
-	if !hasCost {
-		titles = []string{"   ", "RTT STD.     ", "RTT Avg.     ", "Hit  "}
-	}
-	items := make([]*routing.OutboundInfo, 0)
-	for _, node := range nodes {
-		item := &routing.OutboundInfo{
-			Tag: node.Tag,
-		}
-		var status string
-		cost := fmt.Sprintf("%.2f", s.costs.Get(node.Tag))
-		switch node.applied {
-		case rttFailed:
-			status = "x"
-		case rttUntested:
-			status = "?"
-		case rttUnqualified:
-			status = ">"
-		default:
-			status = "OK"
-		}
-		if hasCost {
-			item.Values = []string{
-				status,
-				durationString(node.RTTDeviationCost),
-				durationString(node.RTTDeviation),
-				durationString(node.RTTAverage),
-				fmt.Sprintf("%d/%d", node.CountAll-node.CountFail, node.CountAll),
-				cost,
-			}
-		} else {
-			item.Values = []string{
-				status,
-				durationString(node.RTTDeviation),
-				durationString(node.RTTAverage),
-				fmt.Sprintf("%d/%d", node.CountAll-node.CountFail, node.CountAll),
-			}
-		}
-		items = append(items, item)
-	}
-	return titles, items
-}
+	outboundlist := outboundList(candidates)
 
-func durationString(d time.Duration) string {
-	if d <= 0 || d > time.Hour {
-		return "-"
+	var ret []*node
+
+	for _, v := range results.Status {
+		if v.Alive && v.Delay < maxRTT.Milliseconds() && outboundlist.contains(v.OutboundTag) {
+			record := &node{
+				Tag:              v.OutboundTag,
+				CountAll:         1,
+				CountFail:        1,
+				RTTAverage:       time.Duration(v.Delay) * time.Millisecond,
+				RTTDeviation:     time.Duration(v.Delay) * time.Millisecond,
+				RTTDeviationCost: time.Duration(s.costs.Apply(v.OutboundTag, float64(time.Duration(v.Delay)*time.Millisecond))),
+			}
+
+			if v.HealthPing != nil {
+				record.RTTAverage = time.Duration(v.HealthPing.Average)
+				record.RTTDeviation = time.Duration(v.HealthPing.Deviation)
+				record.RTTDeviationCost = time.Duration(s.costs.Apply(v.OutboundTag, float64(v.HealthPing.Deviation)))
+				record.CountAll = int(v.HealthPing.All)
+				record.CountFail = int(v.HealthPing.Fail)
+
+			}
+			ret = append(ret, record)
+		}
 	}
-	return d.String()
+
+	leastloadSort(ret)
+	return ret
 }
 
 func leastloadSort(nodes []*node) {
 	sort.Slice(nodes, func(i, j int) bool {
 		left := nodes[i]
 		right := nodes[j]
-		if left.applied != right.applied {
-			return left.applied < right.applied
-		}
 		if left.RTTDeviationCost != right.RTTDeviationCost {
 			return left.RTTDeviationCost < right.RTTDeviationCost
 		}

parhelia512/v2ray-core

app/router/strategy_leastload.go
@@ -4,25 +4,29 @@
 package router
 
 import (
-	"fmt"
+	"context"
 	"math"
 	"sort"
-	"strings"
 	"time"
 
-	"github.com/v2fly/v2ray-core/v4/app/observatory/burst"
+	core "github.com/v2fly/v2ray-core/v4"
+	"github.com/v2fly/v2ray-core/v4/app/observatory"
+	"github.com/v2fly/v2ray-core/v4/common"
 	"github.com/v2fly/v2ray-core/v4/common/dice"
-	"github.com/v2fly/v2ray-core/v4/features/routing"
 )
 
 // LeastLoadStrategy represents a least load balancing strategy
 type LeastLoadStrategy struct {
 	settings *StrategyLeastLoadConfig
 	costs    *WeightManager
+
+	observer *observatory.Observer
+
+	ctx context.Context
 }
 
 // NewLeastLoadStrategy creates a new LeastLoadStrategy with settings
-func NewLeastLoadStrategy(settings *StrategyLeastLoadConfig, dispatcher routing.Dispatcher) *LeastLoadStrategy {
+func NewLeastLoadStrategy(settings *StrategyLeastLoadConfig) *LeastLoadStrategy {
 	return &LeastLoadStrategy{
 		settings: settings,
 		costs: NewWeightManager(
@@ -44,30 +48,18 @@ type node struct {
 	RTTAverage       time.Duration
 	RTTDeviation     time.Duration
 	RTTDeviationCost time.Duration
-
-	applied time.Duration
 }
 
-// GetInformation implements the routing.BalancingStrategy.
-func (l *LeastLoadStrategy) GetInformation(tags []string) *routing.StrategyInfo {
-	qualified, others := l.getNodes(tags, time.Duration(l.settings.MaxRTT))
-	selects := l.selectLeastLoad(qualified)
-	// append qualified but not selected outbounds to others
-	others = append(others, qualified[len(selects):]...)
-	leastloadSort(others)
-	titles, sl := l.getNodesInfo(selects)
-	_, ot := l.getNodesInfo(others)
-	return &routing.StrategyInfo{
-		Settings:    l.getSettings(),
-		ValueTitles: titles,
-		Selects:     sl,
-		Others:      ot,
-	}
+func (l *LeastLoadStrategy) InjectContext(ctx context.Context) {
+	l.ctx = ctx
+
+	common.Must(core.RequireFeatures(ctx, func(observerInstance *observatory.Observer) {
+		l.observer = observerInstance
+	}))
 }
 
-// SelectAndPick implements the routing.BalancingStrategy.
-func (l *LeastLoadStrategy) SelectAndPick(candidates []string) string {
-	qualified, _ := l.getNodes(candidates, time.Duration(l.settings.MaxRTT))
+func (l *LeastLoadStrategy) PickOutbound(candidates []string) string {
+	qualified := l.getNodes(candidates, time.Duration(l.settings.MaxRTT))
 	selects := l.selectLeastLoad(qualified)
 	count := len(selects)
 	if count == 0 {
@@ -77,19 +69,9 @@ func (l *LeastLoadStrategy) SelectAndPick(candidates []string) string {
 	return selects[dice.Roll(count)].Tag
 }
 
-// Pick implements the routing.BalancingStrategy.
-func (_ *LeastLoadStrategy) Pick(candidates []string) string {
-	count := len(candidates)
-	if count == 0 {
-		// goes to fallbackTag
-		return ""
-	}
-	return candidates[dice.Roll(count)]
-}
-
 // selectLeastLoad selects nodes according to Baselines and Expected Count.
 //
-// The strategy always improves network response speed, not matter which mode below is configurated.
+// The strategy always improves network response speed, not matter which mode below is configured.
 // But they can still have different priorities.
 //
 // 1. Bandwidth priority: no Baseline + Expected Count > 0.: selects `Expected Count` of nodes.
@@ -126,7 +108,7 @@ func (l *LeastLoadStrategy) selectLeastLoad(nodes []*node) []*node {
 	for _, b := range l.settings.Baselines {
 		baseline := time.Duration(b)
 		for i := 0; i < availableCount; i++ {
-			if nodes[i].applied > baseline {
+			if nodes[i].RTTDeviationCost > baseline {
 				break
 			}
 			count = i + 1
@@ -143,163 +125,48 @@ func (l *LeastLoadStrategy) selectLeastLoad(nodes []*node) []*node {
 	return nodes[:count]
 }
 
-func (l *LeastLoadStrategy) getNodes(candidates []string, maxRTT time.Duration) ([]*node, []*node) {
-	l.access.Lock()
-	defer l.access.Unlock()
-	results := l.Results
-	qualified := make([]*node, 0)
-	unqualified := make([]*node, 0)
-	failed := make([]*node, 0)
-	untested := make([]*node, 0)
-	others := make([]*node, 0)
-	for _, tag := range candidates {
-		r, ok := results[tag]
-		if !ok {
-			untested = append(untested, &node{
-				Tag:              tag,
-				RTTDeviationCost: 0,
-				RTTDeviation:     0,
-				RTTAverage:       0,
-				applied:          rttUntested,
-			})
-			continue
-		}
-		stats := r.Get()
-		node := &node{
-			Tag:              tag,
-			RTTDeviationCost: time.Duration(l.costs.Apply(tag, float64(stats.Deviation))),
-			RTTDeviation:     stats.Deviation,
-			RTTAverage:       stats.Average,
-			CountAll:         stats.All,
-			CountFail:        stats.Fail,
-		}
-		switch {
-		case stats.All == 0:
-			node.applied = rttUntested
-			untested = append(untested, node)
-		case maxRTT > 0 && stats.Average > maxRTT:
-			node.applied = rttUnqualified
-			unqualified = append(unqualified, node)
-		case float64(stats.Fail)/float64(stats.All) > float64(l.settings.Tolerance):
-			node.applied = rttFailed
-			if stats.All-stats.Fail == 0 {
-				// no good, put them after has-good nodes
-				node.RTTDeviationCost = rttFailed
-				node.RTTDeviation = rttFailed
-				node.RTTAverage = rttFailed
-			}
-			failed = append(failed, node)
-		default:
-			node.applied = node.RTTDeviationCost
-			qualified = append(qualified, node)
-		}
+func (l *LeastLoadStrategy) getNodes(candidates []string, maxRTT time.Duration) []*node {
+	observeResult, err := l.observer.GetObservation(l.ctx)
+	if err != nil {
+		newError("cannot get observation").Base(err)
 	}
-	if len(qualified) > 0 {
-		leastloadSort(qualified)
-		others = append(others, unqualified...)
-		others = append(others, untested...)
-		others = append(others, failed...)
-	} else {
-		qualified = untested
-		others = append(others, unqualified...)
-		others = append(others, failed...)
-	}
-	return qualified, others
-}
 
-func (l *LeastLoadStrategy) getSettings() []string {
-	settings := make([]string, 0)
-	sb := new(strings.Builder)
-	for i, b := range l.settings.Baselines {
-		if i > 0 {
-			sb.WriteByte(' ')
-		}
-		sb.WriteString(time.Duration(b).String())
-	}
-	baselines := sb.String()
-	if baselines == "" {
-		baselines = "none"
-	}
-	maxRTT := time.Duration(l.settings.MaxRTT).String()
-	if l.settings.MaxRTT == 0 {
-		maxRTT = "none"
-	}
-	settings = append(settings, fmt.Sprintf(
-		"leastload, expected: %d, baselines: %s, max rtt: %s, tolerance: %.2f",
-		l.settings.Expected,
-		baselines,
-		maxRTT,
-		l.settings.Tolerance,
-	))
-	settings = append(settings, fmt.Sprintf(
-		"health ping, interval: %s, sampling: %d, timeout: %s, destination: %s",
-		l.HealthPing.Settings.Interval,
-		l.HealthPing.Settings.SamplingCount,
-		l.HealthPing.Settings.Timeout,
-		l.HealthPing.Settings.Destination,
-	))
-	return settings
-}
+	results := observeResult.(*observatory.ObservationResult)
 
-func (l *LeastLoadStrategy) getNodesInfo(nodes []*node) ([]string, []*routing.OutboundInfo) {
-	titles := []string{"   ", "RTT STD+C    ", "RTT STD.     ", "RTT Avg.     ", "Hit  ", "Cost "}
-	hasCost := len(l.settings.Costs) > 0
-	if !hasCost {
-		titles = []string{"   ", "RTT STD.     ", "RTT Avg.     ", "Hit  "}
-	}
-	items := make([]*routing.OutboundInfo, 0)
-	for _, node := range nodes {
-		item := &routing.OutboundInfo{
-			Tag: node.Tag,
-		}
-		var status string
-		cost := fmt.Sprintf("%.2f", l.costs.Get(node.Tag))
-		switch node.applied {
-		case rttFailed:
-			status = "x"
-		case rttUntested:
-			status = "?"
-		case rttUnqualified:
-			status = ">"
-		default:
-			status = "OK"
-		}
-		if hasCost {
-			item.Values = []string{
-				status,
-				durationString(node.RTTDeviationCost),
-				durationString(node.RTTDeviation),
-				durationString(node.RTTAverage),
-				fmt.Sprintf("%d/%d", node.CountAll-node.CountFail, node.CountAll),
-				cost,
-			}
-		} else {
-			item.Values = []string{
-				status,
-				durationString(node.RTTDeviation),
-				durationString(node.RTTAverage),
-				fmt.Sprintf("%d/%d", node.CountAll-node.CountFail, node.CountAll),
-			}
-		}
-		items = append(items, item)
-	}
-	return titles, items
-}
+	outboundlist := outboundList(candidates)
 
-func durationString(d time.Duration) string {
-	if d <= 0 || d > time.Hour {
-		return "-"
+	var ret []*node
+
+	for _, v := range results.Status {
+		if v.Alive && v.Delay < maxRTT.Milliseconds() && outboundlist.contains(v.OutboundTag) {
+			record := &node{
+				Tag:              v.OutboundTag,
+				CountAll:         1,
+				CountFail:        1,
+				RTTAverage:       time.Duration(v.Delay) * time.Millisecond,
+				RTTDeviation:     time.Duration(v.Delay) * time.Millisecond,
+				RTTDeviationCost: time.Duration(l.costs.Apply(v.OutboundTag, float64(time.Duration(v.Delay)*time.Millisecond))),
+			}
+
+			if v.HealthPing != nil {
+				record.RTTAverage = time.Duration(v.HealthPing.Average)
+				record.RTTDeviation = time.Duration(v.HealthPing.Deviation)
+				record.RTTDeviationCost = time.Duration(l.costs.Apply(v.OutboundTag, float64(v.HealthPing.Deviation)))
+				record.CountAll = int(v.HealthPing.All)
+				record.CountFail = int(v.HealthPing.Fail)
+			}
+			ret = append(ret, record)
+		}
 	}
-	return d.String()
+
+	leastloadSort(ret)
+	return ret
 }
 
 func leastloadSort(nodes []*node) {
 	sort.Slice(nodes, func(i, j int) bool {
 		left := nodes[i]
 		right := nodes[j]
-		if left.applied != right.applied {
-			return left.applied < right.applied
-		}
 		if left.RTTDeviationCost != right.RTTDeviationCost {
 			return left.RTTDeviationCost < right.RTTDeviationCost
 		}