b7f90b2541346a759e305ca124786add2739d273
秋のかえで <autmaple@protonmail.com>
2022-07-15 11:13:44 +0800

    Chore: bump github.com/lucas-clemente/quic-go from 0.27.0 to 0.28.0

--- transport/internet/quic/dialer.go
+++ transport/internet/quic/dialer.go
@@ -156,7 +156,7 @@ func (s *clientConnections) openConnection(destAddr net.Addr, config *Config, tl
 		ConnectionIDLength:   12,
 		HandshakeIdleTimeout: time.Second * 8,
 		MaxIdleTimeout:       time.Second * 30,
-		KeepAlive:            true,
+		KeepAlivePeriod:      time.Second * 15,
 	}
 
 	sysConn, err := wrapSysConn(rawConn.(*net.UDPConn), config)
