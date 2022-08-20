87212ba450cdd4d2094cc23e9535c278e08be4f3
秋のかえで <autmaple@protonmail.com>
2022-08-20 17:15:20 +0800

    Style: format imports

--- proxy/trojan/server.go
+++ proxy/trojan/server.go
@@ -15,7 +15,7 @@ import (
 	"github.com/v2fly/v2ray-core/v4/common/errors"
 	"github.com/v2fly/v2ray-core/v4/common/log"
 	"github.com/v2fly/v2ray-core/v4/common/net"
-	packetaddr "github.com/v2fly/v2ray-core/v4/common/net/packetaddr"
+	"github.com/v2fly/v2ray-core/v4/common/net/packetaddr"
 	"github.com/v2fly/v2ray-core/v4/common/protocol"
 	udp_proto "github.com/v2fly/v2ray-core/v4/common/protocol/udp"
 	"github.com/v2fly/v2ray-core/v4/common/retry"
