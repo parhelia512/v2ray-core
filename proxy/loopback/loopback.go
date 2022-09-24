148b2900d6949296779cb6c1843609c77e2a8110
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2022-09-24 13:27:46 +0800

    Remove SkipDNSResolve outside DNS module

--- proxy/loopback/loopback.go
+++ proxy/loopback/loopback.go
@@ -41,7 +41,6 @@ func (l *Loopback) Process(ctx context.Context, link *transport.Link, _ internet
 		dialDest := destination
 
 		content := new(session.Content)
-		content.SkipDNSResolve = true
 
 		ctx = session.ContextWithContent(ctx, content)
 
