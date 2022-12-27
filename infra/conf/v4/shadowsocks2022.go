a18df1a044579cc0f5a9ffcf103addb735f9b91a
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2022-12-27 13:20:25 +0800

    add jsonv4 support for new options

---
+++ infra/conf/v4/shadowsocks2022.go
@@ -0,0 +1,26 @@
+package v4
+
+import (
+	"github.com/golang/protobuf/proto"
+
+	"github.com/v2fly/v2ray-core/v5/infra/conf/cfgcommon"
+	shadowsocks2022 "github.com/v2fly/v2ray-core/v5/proxy/shadowsocks2022"
+)
+
+type Shadowsocks2022Config struct {
+	Method  string             `json:"method"`
+	PSK     []byte             `json:"psk"`
+	IPSK    [][]byte           `json:"iPSK"`
+	Address *cfgcommon.Address `json:"address"`
+	Port    uint16             `json:"port"`
+}
+
+func (c *Shadowsocks2022Config) Build() (proto.Message, error) {
+	config := new(shadowsocks2022.ClientConfig)
+	config.Method = c.Method
+	config.Psk = c.PSK
+	config.Ipsk = c.IPSK
+	config.Address = c.Address.Build()
+	config.Port = uint32(c.Port)
+	return config, nil
+}
