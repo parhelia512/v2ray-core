83ad9250f28b31b61fdcecf9cca93851d8fefb81
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2022-12-27 13:20:25 +0800

    add jsonv4 support for new options

--- infra/conf/v4/socks.go
+++ infra/conf/v4/socks.go
@@ -30,12 +30,13 @@ const (
 )
 
 type SocksServerConfig struct {
-	AuthMethod string             `json:"auth"`
-	Accounts   []*SocksAccount    `json:"accounts"`
-	UDP        bool               `json:"udp"`
-	Host       *cfgcommon.Address `json:"ip"`
-	Timeout    uint32             `json:"timeout"`
-	UserLevel  uint32             `json:"userLevel"`
+	AuthMethod     string                   `json:"auth"`
+	Accounts       []*SocksAccount          `json:"accounts"`
+	UDP            bool                     `json:"udp"`
+	Host           *cfgcommon.Address       `json:"ip"`
+	Timeout        uint32                   `json:"timeout"`
+	UserLevel      uint32                   `json:"userLevel"`
+	PacketEncoding cfgcommon.PacketAddrType `json:"packetEncoding"`
 }
 
 func (v *SocksServerConfig) Build() (proto.Message, error) {
@@ -64,6 +65,9 @@ func (v *SocksServerConfig) Build() (proto.Message, error) {
 
 	config.Timeout = v.Timeout
 	config.UserLevel = v.UserLevel
+
+	config.PacketEncoding = v.PacketEncoding.Build()
+
 	return config, nil
 }
 
