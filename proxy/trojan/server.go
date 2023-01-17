6ecf736baea98548c682f5ca01f34f5dfb01153a
MP <112455607+asiaqa@users.noreply.github.com>
2023-01-17 11:18:58 +0800

    Trojan, VLESS: Use buf.FromBytes(make([]byte, buf.Size)) to create first buffer

--- proxy/trojan/server.go
+++ proxy/trojan/server.go
@@ -145,8 +145,8 @@ func (s *Server) Process(ctx context.Context, network net.Network, conn internet
 		return newError("unable to set read deadline").Base(err).AtWarning()
 	}
 
-	first := buf.New()
-	defer first.Release()
+	first := buf.FromBytes(make([]byte, buf.Size))
+	first.Clear()
 
 	firstLen, err := first.ReadFrom(conn)
 	if err != nil {
