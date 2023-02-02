9d0a66b8ff9dea2353577de361739dc32e313667
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2023-02-02 19:00:33 +0800

    update dependencies

--- proxy/shadowsocks2022/client_session.go
+++ proxy/shadowsocks2022/client_session.go
@@ -12,7 +12,7 @@ import (
 	"github.com/v2fly/v2ray-core/v5/common/net"
 	"github.com/v2fly/v2ray-core/v5/transport/internet"
 
-	"github.com/pion/transport/v2/replaydetector"
+	"github.com/pion/transport/v3/replaydetector"
 )
 
 func NewClientUDPSession(ctx context.Context, conn io.ReadWriteCloser, packetProcessor UDPClientPacketProcessor) *ClientUDPSession {
