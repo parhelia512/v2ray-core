fd8c5952378c6fc593b25d1557def0085e4fb73d
AkinoKaede <i@akinokae.de>
2023-05-28 12:18:58 +0800

    feat: add tun support for tcp

---
+++ app/tun/device/device.go
@@ -0,0 +1,22 @@
+//go:build !confonly
+// +build !confonly
+
+package device
+
+import (
+	"github.com/v2fly/v2ray-core/v4/common"
+	"gvisor.dev/gvisor/pkg/tcpip/stack"
+)
+
+type Device interface {
+	stack.LinkEndpoint
+
+	common.Closable
+}
+
+type Options struct {
+	Name string
+	MTU  uint32
+}
+
+type NewTUNFunc func(Options) (Device, error)
