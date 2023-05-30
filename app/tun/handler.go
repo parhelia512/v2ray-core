246165719c0a08e84c2bd6c37c57867726a28682
AkinoKaede <i@akinokae.de>
2023-05-30 13:28:22 +0800

    feat: refine handle logic

--- app/tun/handler.go
+++ app/tun/handler.go
@@ -2,20 +2,3 @@
 // +build !confonly
 
 package tun
-
-import (
-	tun_net "github.com/v2fly/v2ray-core/v4/app/tun/net"
-)
-
-var (
-	tcpQueue = make(chan tun_net.TCPConn)
-	udpQueue = make(chan tun_net.UDPConn)
-)
-
-func handleTCP(conn tun_net.TCPConn) {
-	tcpQueue <- conn
-}
-
-func handleUDP(conn tun_net.UDPConn) {
-	udpQueue <- conn
-}
