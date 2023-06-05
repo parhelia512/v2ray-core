92af48ca27d21f7a221e4fba862b984c79952e82
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2023-06-06 01:22:12 +0800

    Add DoH3 client and DoQ remote client

--- app/dns/nameserver_doh.go
+++ app/dns/nameserver_doh.go
@@ -40,12 +40,13 @@ type DoHNameServer struct {
 	httpClient *http.Client
 	dohURL     string
 	name       string
+	protocol   string
 }
 
 // NewDoHNameServer creates DOH server object for remote resolving.
 func NewDoHNameServer(url *url.URL, dispatcher routing.Dispatcher) (*DoHNameServer, error) {
 	newError("DNS: created Remote DOH client for ", url.String()).AtInfo().WriteToLog()
-	s := baseDOHNameServer(url, "DOH")
+	s := baseDOHNameServer(url, "DOH", "tls")
 
 	tr := &http.Transport{
 		MaxIdleConns:        30,
@@ -94,7 +95,7 @@ func NewDoHNameServer(url *url.URL, dispatcher routing.Dispatcher) (*DoHNameServ
 // NewDoHLocalNameServer creates DOH client object for local resolving
 func NewDoHLocalNameServer(url *url.URL) *DoHNameServer {
 	url.Scheme = "https"
-	s := baseDOHNameServer(url, "DOHL")
+	s := baseDOHNameServer(url, "DOHL", "tls")
 	tr := &http.Transport{
 		IdleConnTimeout:   90 * time.Second,
 		ForceAttemptHTTP2: true,
@@ -118,12 +119,13 @@ func NewDoHLocalNameServer(url *url.URL) *DoHNameServer {
 	return s
 }
 
-func baseDOHNameServer(url *url.URL, prefix string) *DoHNameServer {
+func baseDOHNameServer(url *url.URL, prefix string, protocol string) *DoHNameServer {
 	s := &DoHNameServer{
-		ips:    make(map[string]record),
-		pub:    pubsub.NewService(),
-		name:   prefix + "//" + url.Host,
-		dohURL: url.String(),
+		ips:      make(map[string]record),
+		pub:      pubsub.NewService(),
+		name:     prefix + "//" + url.Host,
+		dohURL:   url.String(),
+		protocol: protocol,
 	}
 	s.cleanup = &task.Periodic{
 		Interval: time.Minute,
@@ -239,7 +241,7 @@ func (s *DoHNameServer) sendQuery(ctx context.Context, domain string, clientIP n
 			}
 
 			dnsCtx = session.ContextWithContent(dnsCtx, &session.Content{
-				Protocol:       "tls",
+				Protocol:       s.protocol,
 				SkipDNSResolve: true,
 			})
 
