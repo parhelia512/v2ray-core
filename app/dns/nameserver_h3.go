e407837f78d8dccaa297c972a719de6a17733c57
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2023-06-06 01:22:12 +0800

    Add DoH3 client and DoQ remote client

---
+++ app/dns/nameserver_h3.go
@@ -0,0 +1,99 @@
+package dns
+
+import (
+	"context"
+	"crypto/tls"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/quic-go/quic-go"
+	"github.com/quic-go/quic-go/http3"
+
+	core "github.com/v2fly/v2ray-core/v5"
+	"github.com/v2fly/v2ray-core/v5/common/net"
+	"github.com/v2fly/v2ray-core/v5/common/net/cnc"
+	"github.com/v2fly/v2ray-core/v5/common/uuid"
+	"github.com/v2fly/v2ray-core/v5/features/routing"
+	"github.com/v2fly/v2ray-core/v5/transport/internet"
+)
+
+// NewH3NameServer creates DOH server object for remote resolving.
+func NewH3NameServer(url *url.URL, dispatcher routing.Dispatcher) (*DoHNameServer, error) {
+	url.Scheme = "https"
+	s := baseDOHNameServer(url, "H3", "quic")
+	tr := &http3.RoundTripper{
+		Dial: func(ctx context.Context, addr string, tlsCfg *tls.Config, cfg *quic.Config) (quic.EarlyConnection, error) {
+			dest, err := net.ParseDestination("udp:" + addr)
+			if err != nil {
+				return nil, err
+			}
+			ctx = core.ToBackgroundDetachedContext(ctx)
+			link, err := dispatcher.Dispatch(ctx, dest)
+			if err != nil {
+				return nil, err
+			}
+			conn := cnc.NewConnection(
+				cnc.ConnectionInputMulti(link.Writer),
+				cnc.ConnectionOutputMultiUDP(link.Reader),
+			)
+			tr := quic.Transport{
+				Conn: &connWrapper{conn},
+			}
+			return tr.DialEarly(ctx, conn.RemoteAddr(), tlsCfg, cfg)
+		},
+	}
+	dispatchedClient := &http.Client{
+		Transport: tr,
+	}
+	s.httpClient = dispatchedClient
+	newError("DNS: created Remote H3 client for ", url.String()).AtInfo().WriteToLog()
+	return s, nil
+}
+
+// NewH3LocalNameServer creates H3 client object for local resolving
+func NewH3LocalNameServer(url *url.URL) *DoHNameServer {
+	url.Scheme = "https"
+	s := baseDOHNameServer(url, "H3L", "quic")
+	tr := &http3.RoundTripper{
+		Dial: func(ctx context.Context, addr string, tlsCfg *tls.Config, cfg *quic.Config) (quic.EarlyConnection, error) {
+			dest, err := net.ParseDestination("udp:" + addr)
+			if err != nil {
+				return nil, err
+			}
+			conn, err := internet.DialSystem(ctx, dest, nil)
+			if err != nil {
+				return nil, err
+			}
+			tr := quic.Transport{
+				Conn: conn.(*internet.PacketConnWrapper).Conn.(*net.UDPConn),
+			}
+			return tr.DialEarly(ctx, conn.RemoteAddr(), tlsCfg, cfg)
+		},
+	}
+	s.httpClient = &http.Client{
+		Timeout:   time.Second * 180,
+		Transport: tr,
+	}
+	newError("DNS: created Local H3 client for ", url.String()).AtInfo().WriteToLog()
+	return s
+}
+
+type connWrapper struct {
+	net.Conn
+}
+
+func (c *connWrapper) ReadFrom(p []byte) (n int, addr net.Addr, err error) {
+	n, err = c.Read(p)
+	return n, c.RemoteAddr(), err
+}
+
+func (c *connWrapper) WriteTo(p []byte, _ net.Addr) (n int, err error) {
+	return c.Write(p)
+}
+
+func (c *connWrapper) LocalAddr() net.Addr {
+	// https://github.com/quic-go/quic-go/commit/8189e75be6121fdc31dc1d6085f17015e9154667#diff-4c6aaadced390f3ce9bec0a9c9bb5203d5fa85df79023e3e0eec423dc9baa946R48-R62
+	uuid := uuid.New()
+	return &net.UnixAddr{Name: uuid.String()}
+}
