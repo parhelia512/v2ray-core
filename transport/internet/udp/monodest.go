d9f60bfee7c46a53f9b34964ae56d3432a594a44
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2023-11-25 00:17:38 +0800

    Add buffer.Endpoint support for MonoDestUDPConn

--- transport/internet/udp/monodest.go
+++ transport/internet/udp/monodest.go
@@ -6,6 +6,19 @@ import (
 	"github.com/v2fly/v2ray-core/v5/transport/internet"
 )
 
+type MonoDestUDPAddr struct {
+	Address net.Address
+	Port    net.Port
+}
+
+func (*MonoDestUDPAddr) Network() string {
+	return "udp"
+}
+
+func (a *MonoDestUDPAddr) String() string {
+	return a.Address.String() + ":" + a.Port.String()
+}
+
 func NewMonoDestUDPConn(conn internet.AbstractPacketConn, addr net.Addr) *MonoDestUDPConn {
 	return &MonoDestUDPConn{
 		AbstractPacketConn: conn,
@@ -21,18 +34,50 @@ type MonoDestUDPConn struct {
 func (m *MonoDestUDPConn) ReadMultiBuffer() (buf.MultiBuffer, error) {
 	buffer := buf.New()
 	buffer.Extend(buf.Size)
-	nBytes, _, err := m.AbstractPacketConn.ReadFrom(buffer.Bytes())
+	nBytes, addr, err := m.AbstractPacketConn.ReadFrom(buffer.Bytes())
 	if err != nil {
 		buffer.Release()
 		return nil, err
 	}
 	buffer.Resize(0, int32(nBytes))
+	if udpAddr, ok := addr.(*net.UDPAddr); ok {
+		buffer.Endpoint = &net.Destination{
+			Address: net.IPAddress(udpAddr.IP),
+			Port:    net.Port(udpAddr.Port),
+			Network: net.Network_UDP,
+		}
+	} else if monoDestUDPAddr, ok := addr.(*MonoDestUDPAddr); ok {
+		buffer.Endpoint = &net.Destination{
+			Address: monoDestUDPAddr.Address,
+			Port:    monoDestUDPAddr.Port,
+			Network: net.Network_UDP,
+		}
+	} else {
+		dest, err := net.ParseDestination(addr.Network() + ":" + addr.String())
+		if err != nil {
+			buffer.Release()
+			return nil, newError("unable to parse destination").Base(err)
+		}
+		buffer.Endpoint = &net.Destination{
+			Address: dest.Address,
+			Port:    dest.Port,
+			Network: net.Network_UDP,
+		}
+	}
 	return buf.MultiBuffer{buffer}, nil
 }
 
 func (m *MonoDestUDPConn) WriteMultiBuffer(buffer buf.MultiBuffer) error {
 	for _, b := range buffer {
-		_, err := m.AbstractPacketConn.WriteTo(b.Bytes(), m.dest)
+		dest := m.dest
+		if b.Endpoint != nil {
+			if !b.Endpoint.Address.Family().IsDomain() {
+				dest = &net.UDPAddr{IP: b.Endpoint.Address.IP(), Port: int(b.Endpoint.Port)}
+			} else {
+				dest = &MonoDestUDPAddr{Address: b.Endpoint.Address, Port: b.Endpoint.Port}
+			}
+		}
+		_, err := m.AbstractPacketConn.WriteTo(b.Bytes(), dest)
 		if err != nil {
 			return err
 		}
