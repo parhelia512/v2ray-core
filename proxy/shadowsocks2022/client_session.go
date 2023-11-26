xiaokangwang/v2ray-core-1

fix proxy ss2022 client lints

proxy/shadowsocks2022/client_session.go
@@ -3,13 +3,14 @@ package shadowsocks2022
 import (
 	"context"
 	"crypto/rand"
-	"github.com/v2fly/v2ray-core/v5/common/buf"
-	"github.com/v2fly/v2ray-core/v5/common/net"
-	"github.com/v2fly/v2ray-core/v5/transport/internet"
 	"io"
 	gonet "net"
 	"sync"
 	"time"
+
+	"github.com/v2fly/v2ray-core/v5/common/buf"
+	"github.com/v2fly/v2ray-core/v5/common/net"
+	"github.com/v2fly/v2ray-core/v5/transport/internet"
 )
 
 func NewClientUDPSession(ctx context.Context, conn io.ReadWriteCloser, packetProcessor UDPClientPacketProcessor) *ClientUDPSession {
@@ -165,7 +166,7 @@ func (c *ClientUDPSessionConn) ReadFrom(p []byte) (n int, addr net.Addr, err err
 	case resp := <-c.readChan:
 		n = copy(p, resp.Payload.Bytes())
 		resp.Payload.Release()
-		addr = &net.UDPAddr{IP: resp.Address.IP(), Port: int(resp.Port)}
+		addr = &net.UDPAddr{IP: resp.Address.IP(), Port: resp.Port}
 	}
 	return
 }

proxy/shadowsocks2022/eih_aes.go
@@ -2,9 +2,12 @@ package shadowsocks2022
 
 import (
 	"crypto/subtle"
-	"github.com/lunixbochs/struc"
-	"github.com/v2fly/v2ray-core/v5/common/buf"
 	"io"
+
+	"github.com/lunixbochs/struc"
+
+	"github.com/v2fly/v2ray-core/v5/common/buf"
+
 	"lukechampine.com/blake3"
 )
 

proxy/shadowsocks2022/kdf_blake3.go
@@ -1,31 +1,31 @@
 package shadowsocks2022
 
 import (
-	"github.com/v2fly/v2ray-core/v5/common/buf"
 	"lukechampine.com/blake3"
+
+	"github.com/v2fly/v2ray-core/v5/common/buf"
 )
 
 func newBLAKE3KeyDerivation() *BLAKE3KeyDerivation {
 	return &BLAKE3KeyDerivation{}
 }
 
-type BLAKE3KeyDerivation struct {
-}
+type BLAKE3KeyDerivation struct{}
 
-func (B BLAKE3KeyDerivation) GetSessionSubKey(effectivePsk, Salt []byte, OutKey []byte) error {
+func (b BLAKE3KeyDerivation) GetSessionSubKey(effectivePsk, salt []byte, outKey []byte) error {
 	keyingMaterialBuffer := buf.New()
 	keyingMaterialBuffer.Write(effectivePsk)
-	keyingMaterialBuffer.Write(Salt)
-	blake3.DeriveKey(OutKey, "shadowsocks 2022 session subkey", keyingMaterialBuffer.Bytes())
+	keyingMaterialBuffer.Write(salt)
+	blake3.DeriveKey(outKey, "shadowsocks 2022 session subkey", keyingMaterialBuffer.Bytes())
 	keyingMaterialBuffer.Release()
 	return nil
 }
 
-func (B BLAKE3KeyDerivation) GetIdentitySubKey(effectivePsk, Salt []byte, OutKey []byte) error {
+func (b BLAKE3KeyDerivation) GetIdentitySubKey(effectivePsk, salt []byte, outKey []byte) error {
 	keyingMaterialBuffer := buf.New()
 	keyingMaterialBuffer.Write(effectivePsk)
-	keyingMaterialBuffer.Write(Salt)
-	blake3.DeriveKey(OutKey, "shadowsocks 2022 identity subkey", keyingMaterialBuffer.Bytes())
+	keyingMaterialBuffer.Write(salt)
+	blake3.DeriveKey(outKey, "shadowsocks 2022 identity subkey", keyingMaterialBuffer.Bytes())
 	keyingMaterialBuffer.Release()
 	return nil
 }

parhelia512/v2ray-core

proxy/shadowsocks2022/client_session.go
@@ -6,13 +6,14 @@ package shadowsocks2022
 import (
 	"context"
 	"crypto/rand"
-	"github.com/v2fly/v2ray-core/v4/common/buf"
-	"github.com/v2fly/v2ray-core/v4/common/net"
-	"github.com/v2fly/v2ray-core/v4/transport/internet"
 	"io"
 	gonet "net"
 	"sync"
 	"time"
+
+	"github.com/v2fly/v2ray-core/v4/common/buf"
+	"github.com/v2fly/v2ray-core/v4/common/net"
+	"github.com/v2fly/v2ray-core/v4/transport/internet"
 )
 
 func NewClientUDPSession(ctx context.Context, conn io.ReadWriteCloser, packetProcessor UDPClientPacketProcessor) *ClientUDPSession {
@@ -168,7 +169,7 @@ func (c *ClientUDPSessionConn) ReadFrom(p []byte) (n int, addr net.Addr, err err
 	case resp := <-c.readChan:
 		n = copy(p, resp.Payload.Bytes())
 		resp.Payload.Release()
-		addr = &net.UDPAddr{IP: resp.Address.IP(), Port: int(resp.Port)}
+		addr = &net.UDPAddr{IP: resp.Address.IP(), Port: resp.Port}
 	}
 	return
 }

proxy/shadowsocks2022/eih_aes.go
@@ -5,9 +5,12 @@ package shadowsocks2022
 
 import (
 	"crypto/subtle"
-	"github.com/lunixbochs/struc"
-	"github.com/v2fly/v2ray-core/v4/common/buf"
 	"io"
+
+	"github.com/lunixbochs/struc"
+
+	"github.com/v2fly/v2ray-core/v4/common/buf"
+
 	"lukechampine.com/blake3"
 )
 

proxy/shadowsocks2022/kdf_blake3.go
@@ -4,31 +4,31 @@
 package shadowsocks2022
 
 import (
-	"github.com/v2fly/v2ray-core/v4/common/buf"
 	"lukechampine.com/blake3"
+
+	"github.com/v2fly/v2ray-core/v4/common/buf"
 )
 
 func newBLAKE3KeyDerivation() *BLAKE3KeyDerivation {
 	return &BLAKE3KeyDerivation{}
 }
 
-type BLAKE3KeyDerivation struct {
-}
+type BLAKE3KeyDerivation struct{}
 
-func (B BLAKE3KeyDerivation) GetSessionSubKey(effectivePsk, Salt []byte, OutKey []byte) error {
+func (b BLAKE3KeyDerivation) GetSessionSubKey(effectivePsk, salt []byte, outKey []byte) error {
 	keyingMaterialBuffer := buf.New()
 	keyingMaterialBuffer.Write(effectivePsk)
-	keyingMaterialBuffer.Write(Salt)
-	blake3.DeriveKey(OutKey, "shadowsocks 2022 session subkey", keyingMaterialBuffer.Bytes())
+	keyingMaterialBuffer.Write(salt)
+	blake3.DeriveKey(outKey, "shadowsocks 2022 session subkey", keyingMaterialBuffer.Bytes())
 	keyingMaterialBuffer.Release()
 	return nil
 }
 
-func (B BLAKE3KeyDerivation) GetIdentitySubKey(effectivePsk, Salt []byte, OutKey []byte) error {
+func (b BLAKE3KeyDerivation) GetIdentitySubKey(effectivePsk, salt []byte, outKey []byte) error {
 	keyingMaterialBuffer := buf.New()
 	keyingMaterialBuffer.Write(effectivePsk)
-	keyingMaterialBuffer.Write(Salt)
-	blake3.DeriveKey(OutKey, "shadowsocks 2022 identity subkey", keyingMaterialBuffer.Bytes())
+	keyingMaterialBuffer.Write(salt)
+	blake3.DeriveKey(outKey, "shadowsocks 2022 identity subkey", keyingMaterialBuffer.Bytes())
 	keyingMaterialBuffer.Release()
 	return nil
 }