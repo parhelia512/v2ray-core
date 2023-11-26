xiaokangwang/v2ray-core-1

add shadowsocks2022 tcp client support

proxy/shadowsocks2022/client.go
@@ -1,0 +1,129 @@
+package shadowsocks2022
+
+import (
+	"context"
+	"github.com/v2fly/v2ray-core/v5/common"
+	"github.com/v2fly/v2ray-core/v5/common/buf"
+	"github.com/v2fly/v2ray-core/v5/common/net"
+	"github.com/v2fly/v2ray-core/v5/common/retry"
+	"github.com/v2fly/v2ray-core/v5/common/session"
+	"github.com/v2fly/v2ray-core/v5/common/signal"
+	"github.com/v2fly/v2ray-core/v5/common/task"
+	"github.com/v2fly/v2ray-core/v5/transport"
+	"github.com/v2fly/v2ray-core/v5/transport/internet"
+	"time"
+)
+
+type Client struct {
+	config *ClientConfig
+	ctx    context.Context
+}
+
+func (c *Client) Process(ctx context.Context, link *transport.Link, dialer internet.Dialer) error {
+	outbound := session.OutboundFromContext(ctx)
+	if outbound == nil || !outbound.Target.IsValid() {
+		return newError("target not specified")
+	}
+	destination := outbound.Target
+	network := destination.Network
+
+	var conn internet.Connection
+
+	err := retry.ExponentialBackoff(5, 100).On(func() error {
+		dest := net.TCPDestination(c.config.Address.AsAddress(), net.Port(c.config.Port))
+		dest.Network = network
+		rawConn, err := dialer.Dial(ctx, dest)
+		if err != nil {
+			return err
+		}
+		conn = rawConn
+
+		return nil
+	})
+	if err != nil {
+		return newError("failed to find an available destination").AtWarning().Base(err)
+	}
+	newError("tunneling request to ", destination, " via ", network, ":", c.config.Address).WriteToLog(session.ExportIDToError(ctx))
+	defer conn.Close()
+
+	var keyDerivation = newBLAKE3KeyDerivation()
+	var method Method
+	switch c.config.Method {
+	case "2022-blake3-aes-128-gcm":
+		method = newAES128GCMMethod()
+	case "2022-blake3-aes-256-gcm":
+		method = newAES256GCMMethod()
+	default:
+		return newError("unknown method: ", c.config.Method)
+	}
+
+	effectivePsk := c.config.Psk
+
+	ctx, cancel := context.WithCancel(ctx)
+	timer := signal.CancelAfterInactivity(ctx, cancel, time.Minute)
+
+	if network == net.Network_TCP {
+		request := &TCPRequest{
+			keyDerivation: keyDerivation,
+			method:        method,
+		}
+		TCPRequestBuffer := buf.New()
+		defer TCPRequestBuffer.Release()
+		err = request.EncodeTCPRequestHeader(effectivePsk, c.config.Ipsk, destination.Address,
+			int(destination.Port), nil, TCPRequestBuffer)
+		if err != nil {
+			return newError("failed to encode TCP request header").Base(err)
+		}
+		_, err = conn.Write(TCPRequestBuffer.Bytes())
+		if err != nil {
+			return newError("failed to write TCP request header").Base(err)
+		}
+		requestDone := func() error {
+			encodedWriter := request.CreateClientC2SWriter(conn)
+			return buf.Copy(link.Reader, encodedWriter, buf.UpdateActivity(timer))
+		}
+		responseDone := func() error {
+			err = request.DecodeTCPResponseHeader(effectivePsk, conn)
+			if err != nil {
+				return newError("failed to decode TCP response header").Base(err)
+			}
+			if err = request.CheckC2SConnectionConstraint(); err != nil {
+				return newError("C2S connection constraint violation").Base(err)
+			}
+			initialPayload := buf.NewWithSize(65535)
+			encodedReader, err := request.CreateClientS2CReader(conn, initialPayload)
+			if err != nil {
+				return newError("failed to create client S2C reader").Base(err)
+			}
+			err = link.Writer.WriteMultiBuffer(buf.MultiBuffer{initialPayload})
+			if err != nil {
+				return newError("failed to write initial payload").Base(err)
+			}
+			return buf.Copy(encodedReader, link.Writer, buf.UpdateActivity(timer))
+		}
+		responseDoneAndCloseWriter := task.OnSuccess(responseDone, task.Close(link.Writer))
+		if err := task.Run(ctx, requestDone, responseDoneAndCloseWriter); err != nil {
+			return newError("connection ends").Base(err)
+		}
+		return nil
+	} else {
+		return newError("not implemented")
+	}
+}
+
+func NewClient(ctx context.Context, config *ClientConfig) (*Client, error) {
+	return &Client{
+		config: config,
+		ctx:    ctx,
+	}, nil
+}
+
+func init() {
+	common.Must(common.RegisterConfig((*ClientConfig)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
+		clientConfig, ok := config.(*ClientConfig)
+		if !ok {
+			return nil, newError("not a ClientConfig")
+		}
+		return NewClient(ctx, clientConfig)
+	}))
+}

proxy/shadowsocks2022/errors.generated.go
@@ -1,0 +1,9 @@
+package shadowsocks2022
+
+import "github.com/v2fly/v2ray-core/v5/common/errors"
+
+type errPathObjHolder struct{}
+
+func newError(values ...interface{}) *errors.Error {
+	return errors.New(values...).WithPathObj(errPathObjHolder{})
+}

proxy/shadowsocks2022/kdf_blake3.go
@@ -1,0 +1,31 @@
+package shadowsocks2022
+
+import (
+	"github.com/v2fly/v2ray-core/v5/common/buf"
+	"lukechampine.com/blake3"
+)
+
+func newBLAKE3KeyDerivation() *BLAKE3KeyDerivation {
+	return &BLAKE3KeyDerivation{}
+}
+
+type BLAKE3KeyDerivation struct {
+}
+
+func (B BLAKE3KeyDerivation) GetSessionSubKey(effectivePsk, Salt []byte, OutKey []byte) error {
+	keyingMaterialBuffer := buf.New()
+	keyingMaterialBuffer.Write(effectivePsk)
+	keyingMaterialBuffer.Write(Salt)
+	blake3.DeriveKey(OutKey, "shadowsocks 2022 session subkey", keyingMaterialBuffer.Bytes())
+	keyingMaterialBuffer.Release()
+	return nil
+}
+
+func (B BLAKE3KeyDerivation) GetIdentitySubKey(effectivePsk, Salt []byte, OutKey []byte) error {
+	keyingMaterialBuffer := buf.New()
+	keyingMaterialBuffer.Write(effectivePsk)
+	keyingMaterialBuffer.Write(Salt)
+	blake3.DeriveKey(OutKey, "shadowsocks 2022 identity subkey", keyingMaterialBuffer.Bytes())
+	keyingMaterialBuffer.Release()
+	return nil
+}

proxy/shadowsocks2022/ss2022.go
@@ -1,0 +1,88 @@
+package shadowsocks2022
+
+import (
+	"crypto/cipher"
+	"github.com/lunixbochs/struc"
+	"github.com/v2fly/v2ray-core/v5/common/net"
+	"github.com/v2fly/v2ray-core/v5/common/protocol"
+	"io"
+)
+
+//go:generate go run github.com/v2fly/v2ray-core/v5/common/errors/errorgen
+
+type KeyDerivation interface {
+	GetSessionSubKey(effectivePsk, Salt []byte, OutKey []byte) error
+	GetIdentitySubKey(effectivePsk, Salt []byte, OutKey []byte) error
+}
+
+type Method interface {
+	GetSessionSubKeyAndSaltLength() int
+	GetStreamAEAD(SessionSubKey []byte) (cipher.AEAD, error)
+	GenerateEIH(CurrentIdentitySubKey []byte, nextPskHash []byte, out []byte) error
+}
+
+type ExtensibleIdentityHeaders interface {
+	struc.Custom
+}
+
+type DestinationAddress interface {
+	net.Address
+}
+
+type RequestSalt interface {
+	struc.Custom
+	isRequestSalt()
+	Bytes() []byte
+	FillAllFrom(reader io.Reader) error
+}
+
+type TCPRequestHeader1PreSessionKey struct {
+	Salt RequestSalt
+	EIH  ExtensibleIdentityHeaders
+}
+
+type TCPRequestHeader2FixedLength struct {
+	Type         byte
+	Timestamp    uint64
+	HeaderLength uint16
+}
+
+type TCPRequestHeader3VariableLength struct {
+	DestinationAddress DestinationAddress
+	Contents           struct {
+		PaddingLength uint16 `struc:"sizeof=Padding"`
+		Padding       []byte
+	}
+}
+
+type TCPRequestHeader struct {
+	PreSessionKeyHeader TCPRequestHeader1PreSessionKey
+	FixedLengthHeader   TCPRequestHeader2FixedLength
+	Header              TCPRequestHeader3VariableLength
+}
+
+type TCPResponseHeader1PreSessionKey struct {
+	Salt RequestSalt
+}
+
+type TCPResponseHeader2FixedLength struct {
+	Type                 byte
+	Timestamp            uint64
+	RequestSalt          RequestSalt
+	InitialPayloadLength uint16
+}
+type TCPResponseHeader struct {
+	PreSessionKeyHeader TCPResponseHeader1PreSessionKey
+	Header              TCPResponseHeader2FixedLength
+}
+
+const TCPHeaderTypeClientToServerStream = byte(0x00)
+const TCPHeaderTypeServerToClientStream = byte(0x01)
+const TCPMinPaddingLength = 0
+const TCPMaxPaddingLength = 900
+
+var addrParser = protocol.NewAddressParser(
+	protocol.AddressFamilyByte(0x01, net.AddressFamilyIPv4),
+	protocol.AddressFamilyByte(0x04, net.AddressFamilyIPv6),
+	protocol.AddressFamilyByte(0x03, net.AddressFamilyDomain),
+)

parhelia512/v2ray-core

proxy/shadowsocks2022/client.go
@@ -1,0 +1,132 @@
+//go:build !confonly
+// +build !confonly
+
+package shadowsocks2022
+
+import (
+	"context"
+	"github.com/v2fly/v2ray-core/v4/common"
+	"github.com/v2fly/v2ray-core/v4/common/buf"
+	"github.com/v2fly/v2ray-core/v4/common/net"
+	"github.com/v2fly/v2ray-core/v4/common/retry"
+	"github.com/v2fly/v2ray-core/v4/common/session"
+	"github.com/v2fly/v2ray-core/v4/common/signal"
+	"github.com/v2fly/v2ray-core/v4/common/task"
+	"github.com/v2fly/v2ray-core/v4/transport"
+	"github.com/v2fly/v2ray-core/v4/transport/internet"
+	"time"
+)
+
+type Client struct {
+	config *ClientConfig
+	ctx    context.Context
+}
+
+func (c *Client) Process(ctx context.Context, link *transport.Link, dialer internet.Dialer) error {
+	outbound := session.OutboundFromContext(ctx)
+	if outbound == nil || !outbound.Target.IsValid() {
+		return newError("target not specified")
+	}
+	destination := outbound.Target
+	network := destination.Network
+
+	var conn internet.Connection
+
+	err := retry.ExponentialBackoff(5, 100).On(func() error {
+		dest := net.TCPDestination(c.config.Address.AsAddress(), net.Port(c.config.Port))
+		dest.Network = network
+		rawConn, err := dialer.Dial(ctx, dest)
+		if err != nil {
+			return err
+		}
+		conn = rawConn
+
+		return nil
+	})
+	if err != nil {
+		return newError("failed to find an available destination").AtWarning().Base(err)
+	}
+	newError("tunneling request to ", destination, " via ", network, ":", c.config.Address).WriteToLog(session.ExportIDToError(ctx))
+	defer conn.Close()
+
+	var keyDerivation = newBLAKE3KeyDerivation()
+	var method Method
+	switch c.config.Method {
+	case "2022-blake3-aes-128-gcm":
+		method = newAES128GCMMethod()
+	case "2022-blake3-aes-256-gcm":
+		method = newAES256GCMMethod()
+	default:
+		return newError("unknown method: ", c.config.Method)
+	}
+
+	effectivePsk := c.config.Psk
+
+	ctx, cancel := context.WithCancel(ctx)
+	timer := signal.CancelAfterInactivity(ctx, cancel, time.Minute)
+
+	if network == net.Network_TCP {
+		request := &TCPRequest{
+			keyDerivation: keyDerivation,
+			method:        method,
+		}
+		TCPRequestBuffer := buf.New()
+		defer TCPRequestBuffer.Release()
+		err = request.EncodeTCPRequestHeader(effectivePsk, c.config.Ipsk, destination.Address,
+			int(destination.Port), nil, TCPRequestBuffer)
+		if err != nil {
+			return newError("failed to encode TCP request header").Base(err)
+		}
+		_, err = conn.Write(TCPRequestBuffer.Bytes())
+		if err != nil {
+			return newError("failed to write TCP request header").Base(err)
+		}
+		requestDone := func() error {
+			encodedWriter := request.CreateClientC2SWriter(conn)
+			return buf.Copy(link.Reader, encodedWriter, buf.UpdateActivity(timer))
+		}
+		responseDone := func() error {
+			err = request.DecodeTCPResponseHeader(effectivePsk, conn)
+			if err != nil {
+				return newError("failed to decode TCP response header").Base(err)
+			}
+			if err = request.CheckC2SConnectionConstraint(); err != nil {
+				return newError("C2S connection constraint violation").Base(err)
+			}
+			initialPayload := buf.NewWithSize(65535)
+			encodedReader, err := request.CreateClientS2CReader(conn, initialPayload)
+			if err != nil {
+				return newError("failed to create client S2C reader").Base(err)
+			}
+			err = link.Writer.WriteMultiBuffer(buf.MultiBuffer{initialPayload})
+			if err != nil {
+				return newError("failed to write initial payload").Base(err)
+			}
+			return buf.Copy(encodedReader, link.Writer, buf.UpdateActivity(timer))
+		}
+		responseDoneAndCloseWriter := task.OnSuccess(responseDone, task.Close(link.Writer))
+		if err := task.Run(ctx, requestDone, responseDoneAndCloseWriter); err != nil {
+			return newError("connection ends").Base(err)
+		}
+		return nil
+	} else {
+		return newError("not implemented")
+	}
+}
+
+func NewClient(ctx context.Context, config *ClientConfig) (*Client, error) {
+	return &Client{
+		config: config,
+		ctx:    ctx,
+	}, nil
+}
+
+func init() {
+	common.Must(common.RegisterConfig((*ClientConfig)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
+		clientConfig, ok := config.(*ClientConfig)
+		if !ok {
+			return nil, newError("not a ClientConfig")
+		}
+		return NewClient(ctx, clientConfig)
+	}))
+}

proxy/shadowsocks2022/errors.generated.go
@@ -1,0 +1,9 @@
+package shadowsocks2022
+
+import "github.com/v2fly/v2ray-core/v4/common/errors"
+
+type errPathObjHolder struct{}
+
+func newError(values ...interface{}) *errors.Error {
+	return errors.New(values...).WithPathObj(errPathObjHolder{})
+}

proxy/shadowsocks2022/kdf_blake3.go
@@ -1,0 +1,34 @@
+//go:build !confonly
+// +build !confonly
+
+package shadowsocks2022
+
+import (
+	"github.com/v2fly/v2ray-core/v4/common/buf"
+	"lukechampine.com/blake3"
+)
+
+func newBLAKE3KeyDerivation() *BLAKE3KeyDerivation {
+	return &BLAKE3KeyDerivation{}
+}
+
+type BLAKE3KeyDerivation struct {
+}
+
+func (B BLAKE3KeyDerivation) GetSessionSubKey(effectivePsk, Salt []byte, OutKey []byte) error {
+	keyingMaterialBuffer := buf.New()
+	keyingMaterialBuffer.Write(effectivePsk)
+	keyingMaterialBuffer.Write(Salt)
+	blake3.DeriveKey(OutKey, "shadowsocks 2022 session subkey", keyingMaterialBuffer.Bytes())
+	keyingMaterialBuffer.Release()
+	return nil
+}
+
+func (B BLAKE3KeyDerivation) GetIdentitySubKey(effectivePsk, Salt []byte, OutKey []byte) error {
+	keyingMaterialBuffer := buf.New()
+	keyingMaterialBuffer.Write(effectivePsk)
+	keyingMaterialBuffer.Write(Salt)
+	blake3.DeriveKey(OutKey, "shadowsocks 2022 identity subkey", keyingMaterialBuffer.Bytes())
+	keyingMaterialBuffer.Release()
+	return nil
+}

proxy/shadowsocks2022/ss2022.go
@@ -1,0 +1,91 @@
+//go:build !confonly
+// +build !confonly
+
+package shadowsocks2022
+
+import (
+	"crypto/cipher"
+	"github.com/lunixbochs/struc"
+	"github.com/v2fly/v2ray-core/v4/common/net"
+	"github.com/v2fly/v2ray-core/v4/common/protocol"
+	"io"
+)
+
+//go:generate go run github.com/v2fly/v2ray-core/v4/common/errors/errorgen
+
+type KeyDerivation interface {
+	GetSessionSubKey(effectivePsk, Salt []byte, OutKey []byte) error
+	GetIdentitySubKey(effectivePsk, Salt []byte, OutKey []byte) error
+}
+
+type Method interface {
+	GetSessionSubKeyAndSaltLength() int
+	GetStreamAEAD(SessionSubKey []byte) (cipher.AEAD, error)
+	GenerateEIH(CurrentIdentitySubKey []byte, nextPskHash []byte, out []byte) error
+}
+
+type ExtensibleIdentityHeaders interface {
+	struc.Custom
+}
+
+type DestinationAddress interface {
+	net.Address
+}
+
+type RequestSalt interface {
+	struc.Custom
+	isRequestSalt()
+	Bytes() []byte
+	FillAllFrom(reader io.Reader) error
+}
+
+type TCPRequestHeader1PreSessionKey struct {
+	Salt RequestSalt
+	EIH  ExtensibleIdentityHeaders
+}
+
+type TCPRequestHeader2FixedLength struct {
+	Type         byte
+	Timestamp    uint64
+	HeaderLength uint16
+}
+
+type TCPRequestHeader3VariableLength struct {
+	DestinationAddress DestinationAddress
+	Contents           struct {
+		PaddingLength uint16 `struc:"sizeof=Padding"`
+		Padding       []byte
+	}
+}
+
+type TCPRequestHeader struct {
+	PreSessionKeyHeader TCPRequestHeader1PreSessionKey
+	FixedLengthHeader   TCPRequestHeader2FixedLength
+	Header              TCPRequestHeader3VariableLength
+}
+
+type TCPResponseHeader1PreSessionKey struct {
+	Salt RequestSalt
+}
+
+type TCPResponseHeader2FixedLength struct {
+	Type                 byte
+	Timestamp            uint64
+	RequestSalt          RequestSalt
+	InitialPayloadLength uint16
+}
+type TCPResponseHeader struct {
+	PreSessionKeyHeader TCPResponseHeader1PreSessionKey
+	Header              TCPResponseHeader2FixedLength
+}
+
+const TCPHeaderTypeClientToServerStream = byte(0x00)
+const TCPHeaderTypeServerToClientStream = byte(0x01)
+const TCPMinPaddingLength = 0
+const TCPMaxPaddingLength = 900
+
+var addrParser = protocol.NewAddressParser(
+	protocol.AddressFamilyByte(0x01, net.AddressFamilyIPv4),
+	protocol.AddressFamilyByte(0x04, net.AddressFamilyIPv6),
+	protocol.AddressFamilyByte(0x03, net.AddressFamilyDomain),
+)