9e6af306c62fd64f4425b3496ee7f93f79bf670d
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2023-12-23 14:19:10 +0800

    update dependencies

--- app/observatory/burst/burstobserver.go
+++ app/observatory/burst/burstobserver.go
@@ -2,7 +2,6 @@ package burst
 
 import (
 	"context"
-	"sync"
 
 	"github.com/golang/protobuf/proto"
 
@@ -18,8 +17,7 @@ type Observer struct {
 	config *Config
 	ctx    context.Context
 
-	statusLock sync.Mutex // nolint: structcheck
-	hp         *HealthPing
+	hp *HealthPing
 
 	finished *done.Instance
 
