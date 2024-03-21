e362303bb302b812553ac00ad5ad8bf061797077
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2024-03-21 23:37:27 +0800

    Fix HTTP sniffer

--- common/protocol/http/sniff.go
+++ common/protocol/http/sniff.go
@@ -3,6 +3,7 @@ package http
 import (
 	"bytes"
 	"errors"
+	"net/url"
 	"strings"
 
 	"github.com/v2fly/v2ray-core/v5/common"
@@ -10,7 +11,7 @@ import (
 )
 
 type SniffHeader struct {
-	host string
+	domain string
 }
 
 func (h *SniffHeader) Protocol() string {
@@ -18,7 +19,7 @@ func (h *SniffHeader) Protocol() string {
 }
 
 func (h *SniffHeader) Domain() string {
-	return h.host
+	return h.domain
 }
 
 var (
@@ -42,14 +43,34 @@ func beginWithHTTPMethod(b []byte) error {
 	return errNotHTTPMethod
 }
 
+func parseRequestLine(b []byte) (string, bool) {
+	_, rest, ok1 := strings.Cut(string(b), " ")
+	requestURI, _, ok2 := strings.Cut(rest, " ")
+	if !ok1 || !ok2 {
+		return "", false
+	}
+	return requestURI, true
+}
+
 func SniffHTTP(b []byte) (*SniffHeader, error) {
 	if err := beginWithHTTPMethod(b); err != nil {
 		return nil, err
 	}
 
-	sh := &SniffHeader{}
-
 	headers := bytes.Split(b, []byte{'\n'})
+
+	var host string
+
+	requestURI, ok := parseRequestLine(headers[0])
+	if !ok {
+		return nil, common.ErrNoClue
+	}
+	u, err := url.ParseRequestURI(requestURI)
+	if err != nil {
+		return nil, err
+	}
+	host = u.Host
+
 	for i := 1; i < len(headers); i++ {
 		header := headers[i]
 		if len(header) == 0 {
@@ -60,18 +81,19 @@ func SniffHTTP(b []byte) (*SniffHeader, error) {
 			continue
 		}
 		key := strings.ToLower(string(parts[0]))
-		if key == "host" {
-			rawHost := strings.ToLower(string(bytes.TrimSpace(parts[1])))
-			dest, err := ParseHost(rawHost, net.Port(80))
-			if err != nil {
-				return nil, err
-			}
-			sh.host = dest.Address.String()
+		if key == "host" && host == "" {
+			host = string(bytes.TrimSpace(parts[1]))
 		}
 	}
 
-	if len(sh.host) > 0 {
-		return sh, nil
+	if len(host) > 0 {
+		dest, err := ParseHost(host, net.Port(80))
+		if err != nil {
+			return nil, err
+		}
+		if dest.Address.Family().IsDomain() {
+			return &SniffHeader{domain: dest.Address.String()}, nil
+		}
 	}
 
 	return nil, common.ErrNoClue
