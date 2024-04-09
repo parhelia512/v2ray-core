2fe26771f33127f895f4718a9bd7c8cb4d655791
JimmyHuang <jimmyhuang454@gmail.com>
2024-04-10 00:35:39 +0800

    Add Hysteria2 Protocol

---
+++ transport/internet/hysteria2/hub.go
@@ -0,0 +1,132 @@
+package hysteria2
+
+import (
+	"context"
+
+	hy_server "github.com/apernet/hysteria/core/server"
+	"github.com/apernet/quic-go"
+	"github.com/apernet/quic-go/http3"
+
+	"github.com/v2fly/v2ray-core/v5/common"
+	"github.com/v2fly/v2ray-core/v5/common/net"
+	"github.com/v2fly/v2ray-core/v5/common/protocol/tls/cert"
+	"github.com/v2fly/v2ray-core/v5/transport/internet"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/tls"
+)
+
+// Listener is an internet.Listener that listens for TCP connections.
+type Listener struct {
+	hyServer hy_server.Server
+	rawConn  net.PacketConn
+	addConn  internet.ConnHandler
+}
+
+// Addr implements internet.Listener.Addr.
+func (l *Listener) Addr() net.Addr {
+	return l.rawConn.LocalAddr()
+}
+
+// Close implements internet.Listener.Close.
+func (l *Listener) Close() error {
+	return l.hyServer.Close()
+}
+
+func (l *Listener) ProxyStreamHijacker(ft http3.FrameType, conn quic.Connection, stream quic.Stream, err error) (bool, error) {
+	// err always == nil
+	tcpConn := &HyConn{
+		stream: stream,
+		local:  conn.LocalAddr(),
+		remote: conn.RemoteAddr(),
+	}
+	l.addConn(tcpConn)
+	return true, nil
+}
+
+func (l *Listener) UDPHijacker(entry *hy_server.UdpSessionEntry, originalAddr string) {
+	addr, err := net.ResolveUDPAddr("udp", originalAddr)
+	if err != nil {
+		return
+	}
+	udpConn := &HyConn{
+		IsUDPExtension:   true,
+		IsServer:         true,
+		ServerUDPSession: entry,
+		remote:           addr,
+		local:            l.rawConn.LocalAddr(),
+	}
+	l.addConn(udpConn)
+}
+
+// Listen creates a new Listener based on configurations.
+func Listen(ctx context.Context, address net.Address, port net.Port, streamSettings *internet.MemoryStreamConfig, handler internet.ConnHandler) (internet.Listener, error) {
+	if address.Family().IsDomain() {
+		return nil, nil
+	}
+
+	config := streamSettings.ProtocolSettings.(*Config)
+	rawConn, err := internet.ListenSystemPacket(context.Background(),
+		&net.UDPAddr{
+			IP:   address.IP(),
+			Port: int(port),
+		}, streamSettings.SocketSettings)
+	if err != nil {
+		return nil, err
+	}
+
+	listener := &Listener{
+		rawConn: rawConn,
+		addConn: handler,
+	}
+
+	tlsSettings := tls.ConfigFromStreamSettings(streamSettings)
+	if tlsSettings == nil {
+		tlsSettings = &tls.Config{
+			Certificate: []*tls.Certificate{tls.ParseCertificate(cert.MustGenerate(nil, cert.DNSNames(internalDomain), cert.CommonName(internalDomain)))},
+		}
+	}
+	tlsConfig := tlsSettings.GetTLSConfig()
+	hyTLSConfig := &hy_server.TLSConfig{
+		Certificates:   tlsConfig.Certificates,
+		GetCertificate: tlsConfig.GetCertificate,
+	}
+
+	hyConfig := &hy_server.Config{
+		Conn:                  rawConn,
+		TLSConfig:             *hyTLSConfig,
+		Authenticator:         &Authenticator{Password: config.GetPassword()},
+		IgnoreClientBandwidth: config.GetIgnoreClientBandwidth(),
+		DisableUDP:            !config.GetUseUdpExtension(),
+		StreamHijacker:        listener.ProxyStreamHijacker, // acceptStreams
+		UdpSessionHijacker:    listener.UDPHijacker,         // acceptUDPSession
+	}
+	if config.Obfs != nil && config.Obfs.Type == "salamander" {
+		ob, err := NewSalamanderObfuscator([]byte(config.Obfs.Password))
+		if err != nil {
+			return nil, err
+		}
+		hyConfig.Conn = WrapPacketConn(rawConn, ob)
+	}
+	hyServer, err := hy_server.NewServer(hyConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	listener.hyServer = hyServer
+	go hyServer.Serve()
+	return listener, nil
+}
+
+type Authenticator struct {
+	Password string
+}
+
+func (a *Authenticator) Authenticate(addr net.Addr, auth string, tx uint64) (ok bool, id string) {
+	if auth == a.Password || a.Password == "" {
+		return true, "user"
+	}
+	return false, ""
+}
+
+func init() {
+	common.Must(internet.RegisterTransportListener(protocolName, Listen))
+}
