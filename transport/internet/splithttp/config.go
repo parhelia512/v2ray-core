fdcb6c47377bda4c1f803f132b81c8533b8214c9
mmmray <142015632+mmmray@users.noreply.github.com>
2024-06-17 11:53:46 +0800

    import splithttp transport

---
+++ transport/internet/splithttp/config.go
@@ -0,0 +1,43 @@
+package splithttp
+
+import (
+	"net/http"
+)
+
+func (c *Config) GetNormalizedPath() string {
+	path := c.Path
+	if path == "" {
+		path = "/"
+	}
+	if path[0] != '/' {
+		path = "/" + path
+	}
+	if path[len(path)-1] != '/' {
+		path = path + "/"
+	}
+	return path
+}
+
+func (c *Config) GetRequestHeader() http.Header {
+	header := http.Header{}
+	for k, v := range c.Header {
+		header.Add(k, v)
+	}
+	return header
+}
+
+func (c *Config) GetNormalizedMaxConcurrentUploads() int32 {
+	if c.MaxConcurrentUploads == 0 {
+		return 10
+	}
+
+	return c.MaxConcurrentUploads
+}
+
+func (c *Config) GetNormalizedMaxUploadSize() int32 {
+	if c.MaxUploadSize == 0 {
+		return 1000000
+	}
+
+	return c.MaxUploadSize
+}
