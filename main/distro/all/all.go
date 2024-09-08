xiaokangwang/v2ray-core-1

Add Add Hysteria2 Protocol

main/distro/all/all.go
@@ -53,6 +53,7 @@ import (
 	_ "github.com/v2fly/v2ray-core/v5/proxy/vlite/inbound"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/vlite/outbound"
 
+	_ "github.com/v2fly/v2ray-core/v5/proxy/hysteria2"
 	_ "github.com/v2fly/v2ray-core/v5/proxy/shadowsocks2022"
 
 	// Transports
@@ -80,6 +81,8 @@ import (
 	_ "github.com/v2fly/v2ray-core/v5/transport/internet/dtls"
 
 	_ "github.com/v2fly/v2ray-core/v5/transport/internet/httpupgrade"
+
+	_ "github.com/v2fly/v2ray-core/v5/transport/internet/hysteria2"
 
 	// Transport headers
 	_ "github.com/v2fly/v2ray-core/v5/transport/internet/headers/http"

proxy/hysteria2/protocol.go
@@ -1,0 +1,204 @@
+package hysteria2
+
+import (
+	"io"
+	gonet "net"
+
+	hyProtocol "github.com/apernet/hysteria/core/v2/international/protocol"
+	"github.com/apernet/quic-go/quicvarint"
+
+	"github.com/v2fly/v2ray-core/v5/common/buf"
+	"github.com/v2fly/v2ray-core/v5/common/net"
+	hyTransport "github.com/v2fly/v2ray-core/v5/transport/internet/hysteria2"
+)
+
+// ConnWriter is TCP Connection Writer Wrapper
+type ConnWriter struct {
+	io.Writer
+	Target        net.Destination
+	Account       *MemoryAccount
+	TCPHeaderSent bool
+}
+
+// Write implements io.Writer
+func (c *ConnWriter) Write(p []byte) (n int, err error) {
+	if !c.TCPHeaderSent {
+		if err := c.writeTCPHeader(); err != nil {
+			return 0, newError("failed to write request header").Base(err)
+		}
+	}
+
+	return c.Writer.Write(p)
+}
+
+// WriteMultiBuffer implements buf.Writer
+func (c *ConnWriter) WriteMultiBuffer(mb buf.MultiBuffer) error {
+	defer buf.ReleaseMulti(mb)
+
+	for _, b := range mb {
+		if !b.IsEmpty() {
+			if _, err := c.Write(b.Bytes()); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (c *ConnWriter) WriteTCPHeader() error {
+	if !c.TCPHeaderSent {
+		if err := c.writeTCPHeader(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func QuicLen(s int) int {
+	return int(quicvarint.Len(uint64(s)))
+}
+
+func (c *ConnWriter) writeTCPHeader() error {
+	c.TCPHeaderSent = true
+
+	// TODO: the padding length here should be randomized
+
+	padding := "Jimmy Was Here"
+	paddingLen := len(padding)
+	addressAndPort := c.Target.NetAddr()
+	addressLen := len(addressAndPort)
+	size := QuicLen(addressLen) + addressLen + QuicLen(paddingLen) + paddingLen
+
+	if size > hyProtocol.MaxAddressLength+hyProtocol.MaxPaddingLength {
+		return newError("invalid header length")
+	}
+
+	buf := make([]byte, size)
+	i := hyProtocol.VarintPut(buf, uint64(addressLen))
+	i += copy(buf[i:], addressAndPort)
+	i += hyProtocol.VarintPut(buf[i:], uint64(paddingLen))
+	copy(buf[i:], padding)
+
+	_, err := c.Writer.Write(buf)
+	return err
+}
+
+// PacketWriter UDP Connection Writer Wrapper
+type PacketWriter struct {
+	io.Writer
+	HyConn *hyTransport.HyConn
+	Target net.Destination
+}
+
+// WriteMultiBuffer implements buf.Writer
+func (w *PacketWriter) WriteMultiBuffer(mb buf.MultiBuffer) error {
+	for _, b := range mb {
+		if b.IsEmpty() {
+			continue
+		}
+		if _, err := w.writePacket(b.Bytes(), w.Target); err != nil {
+			buf.ReleaseMulti(mb)
+			return err
+		}
+	}
+
+	return nil
+}
+
+// WriteMultiBufferWithMetadata writes udp packet with destination specified
+func (w *PacketWriter) WriteMultiBufferWithMetadata(mb buf.MultiBuffer, dest net.Destination) error {
+	for _, b := range mb {
+		if b.IsEmpty() {
+			continue
+		}
+		if _, err := w.writePacket(b.Bytes(), dest); err != nil {
+			buf.ReleaseMulti(mb)
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (w *PacketWriter) WriteTo(payload []byte, addr gonet.Addr) (int, error) {
+	dest := net.DestinationFromAddr(addr)
+
+	return w.writePacket(payload, dest)
+}
+
+func (w *PacketWriter) writePacket(payload []byte, dest net.Destination) (int, error) {
+	return w.HyConn.WritePacket(payload, dest)
+}
+
+// ConnReader is TCP Connection Reader Wrapper
+type ConnReader struct {
+	io.Reader
+	Target net.Destination
+}
+
+// Read implements io.Reader
+func (c *ConnReader) Read(p []byte) (int, error) {
+	return c.Reader.Read(p)
+}
+
+// ReadMultiBuffer implements buf.Reader
+func (c *ConnReader) ReadMultiBuffer() (buf.MultiBuffer, error) {
+	b := buf.New()
+	_, err := b.ReadFrom(c)
+	return buf.MultiBuffer{b}, err
+}
+
+// PacketPayload combines udp payload and destination
+type PacketPayload struct {
+	Target net.Destination
+	Buffer buf.MultiBuffer
+}
+
+// PacketReader is UDP Connection Reader Wrapper
+type PacketReader struct {
+	io.Reader
+	HyConn *hyTransport.HyConn
+}
+
+// ReadMultiBuffer implements buf.Reader
+func (r *PacketReader) ReadMultiBuffer() (buf.MultiBuffer, error) {
+	p, err := r.ReadMultiBufferWithMetadata()
+	if p != nil {
+		return p.Buffer, err
+	}
+	return nil, err
+}
+
+// ReadMultiBufferWithMetadata reads udp packet with destination
+func (r *PacketReader) ReadMultiBufferWithMetadata() (*PacketPayload, error) {
+	_, data, dest, err := r.HyConn.ReadPacket()
+	if err != nil {
+		return nil, err
+	}
+	b := buf.FromBytes(data)
+	return &PacketPayload{Target: *dest, Buffer: buf.MultiBuffer{b}}, nil
+}
+
+type PacketConnectionReader struct {
+	reader  *PacketReader
+	payload *PacketPayload
+}
+
+func (r *PacketConnectionReader) ReadFrom(p []byte) (n int, addr gonet.Addr, err error) {
+	if r.payload == nil || r.payload.Buffer.IsEmpty() {
+		r.payload, err = r.reader.ReadMultiBufferWithMetadata()
+		if err != nil {
+			return
+		}
+	}
+
+	addr = &gonet.UDPAddr{
+		IP:   r.payload.Target.Address.IP(),
+		Port: int(r.payload.Target.Port),
+	}
+
+	r.payload.Buffer, n = buf.SplitFirstBytes(r.payload.Buffer, p)
+
+	return
+}

transport/internet/hysteria2/dialer.go
@@ -1,0 +1,199 @@
+package hysteria2
+
+import (
+	"context"
+	"sync"
+
+	hyClient "github.com/apernet/hysteria/core/v2/client"
+	hyProtocol "github.com/apernet/hysteria/core/v2/international/protocol"
+	"github.com/apernet/quic-go/quicvarint"
+
+	"github.com/v2fly/v2ray-core/v5/common"
+	"github.com/v2fly/v2ray-core/v5/common/net"
+	"github.com/v2fly/v2ray-core/v5/common/session"
+	"github.com/v2fly/v2ray-core/v5/transport/internet"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/tls"
+)
+
+var RunningClient map[net.Addr](hyClient.Client)
+var ClientMutex sync.Mutex
+var MBps uint64 = 1000000 / 8 // MByte
+
+func GetClientTLSConfig(streamSettings *internet.MemoryStreamConfig) (*hyClient.TLSConfig, error) {
+	config := tls.ConfigFromStreamSettings(streamSettings)
+	if config == nil {
+		return nil, newError(Hy2MustNeedTLS)
+	}
+	tlsConfig := config.GetTLSConfig()
+
+	return &hyClient.TLSConfig{
+		RootCAs:               tlsConfig.RootCAs,
+		ServerName:            tlsConfig.ServerName,
+		InsecureSkipVerify:    tlsConfig.InsecureSkipVerify,
+		VerifyPeerCertificate: tlsConfig.VerifyPeerCertificate,
+	}, nil
+}
+
+func ResolveAddress(dest net.Destination) (net.Addr, error) {
+	var destAddr *net.UDPAddr
+	if dest.Address.Family().IsIP() {
+		destAddr = &net.UDPAddr{
+			IP:   dest.Address.IP(),
+			Port: int(dest.Port),
+		}
+	} else {
+		addr, err := net.ResolveUDPAddr("udp", dest.NetAddr())
+		if err != nil {
+			return nil, err
+		}
+		destAddr = addr
+	}
+	return destAddr, nil
+}
+
+type connFactory struct {
+	hyClient.ConnFactory
+
+	NewFunc func(addr net.Addr) (net.PacketConn, error)
+}
+
+func (f *connFactory) New(addr net.Addr) (net.PacketConn, error) {
+	return f.NewFunc(addr)
+}
+
+func NewHyClient(serverAddr net.Addr, streamSettings *internet.MemoryStreamConfig) (hyClient.Client, error) {
+	tlsConfig, err := GetClientTLSConfig(streamSettings)
+	if err != nil {
+		return nil, err
+	}
+
+	config := streamSettings.ProtocolSettings.(*Config)
+	client, _, err := hyClient.NewClient(&hyClient.Config{
+		Auth:       config.GetPassword(),
+		TLSConfig:  *tlsConfig,
+		ServerAddr: serverAddr,
+		ConnFactory: &connFactory{
+			NewFunc: func(addr net.Addr) (net.PacketConn, error) {
+				rawConn, err := internet.ListenSystemPacket(context.Background(), &net.UDPAddr{
+					IP:   []byte{0, 0, 0, 0},
+					Port: 0,
+				}, streamSettings.SocketSettings)
+				if err != nil {
+					return nil, err
+				}
+				return rawConn.(*net.UDPConn), nil
+			},
+		},
+		BandwidthConfig: hyClient.BandwidthConfig{MaxTx: config.Congestion.GetUpMbps() * MBps, MaxRx: config.GetCongestion().GetDownMbps() * MBps},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func CloseHyClient(serverAddr net.Addr) error {
+	ClientMutex.Lock()
+	defer ClientMutex.Unlock()
+
+	client, found := RunningClient[serverAddr]
+	if found {
+		delete(RunningClient, serverAddr)
+		return client.Close()
+	}
+	return nil
+}
+
+func GetHyClient(serverAddr net.Addr, streamSettings *internet.MemoryStreamConfig) (hyClient.Client, error) {
+	var err error
+	var client hyClient.Client
+
+	ClientMutex.Lock()
+	client, found := RunningClient[serverAddr]
+	ClientMutex.Unlock()
+	if !found || !CheckHyClientHealthy(client) {
+		if found {
+			// retry
+			CloseHyClient(serverAddr)
+		}
+		client, err = NewHyClient(serverAddr, streamSettings)
+		if err != nil {
+			return nil, err
+		}
+		ClientMutex.Lock()
+		RunningClient[serverAddr] = client
+		ClientMutex.Unlock()
+	}
+	return client, nil
+}
+
+func CheckHyClientHealthy(client hyClient.Client) bool {
+	quicConn := client.GetQuicConn()
+	if quicConn == nil {
+		return false
+	}
+	select {
+	case <-quicConn.Context().Done():
+		return false
+	default:
+	}
+	return true
+}
+
+func Dial(ctx context.Context, dest net.Destination, streamSettings *internet.MemoryStreamConfig) (internet.Connection, error) {
+	config := streamSettings.ProtocolSettings.(*Config)
+
+	serverAddr, err := ResolveAddress(dest)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := GetHyClient(serverAddr, streamSettings)
+	if err != nil {
+		CloseHyClient(serverAddr)
+		return nil, err
+	}
+
+	quicConn := client.GetQuicConn()
+	conn := &HyConn{
+		local:  quicConn.LocalAddr(),
+		remote: quicConn.RemoteAddr(),
+	}
+
+	outbound := session.OutboundFromContext(ctx)
+	network := net.Network_TCP
+	if outbound != nil {
+		network = outbound.Target.Network
+		conn.Target = outbound.Target
+	}
+
+	if network == net.Network_UDP && config.GetUseUdpExtension() { // only hysteria2 can use udpExtension
+		conn.IsUDPExtension = true
+		conn.IsServer = false
+		conn.ClientUDPSession, err = client.UDP()
+		if err != nil {
+			CloseHyClient(serverAddr)
+			return nil, err
+		}
+		return conn, nil
+	}
+
+	conn.stream, err = client.OpenStream()
+	if err != nil {
+		CloseHyClient(serverAddr)
+		return nil, err
+	}
+
+	// write TCP frame type
+	frameSize := int(quicvarint.Len(hyProtocol.FrameTypeTCPRequest))
+	buf := make([]byte, frameSize)
+	hyProtocol.VarintPut(buf, hyProtocol.FrameTypeTCPRequest)
+	conn.stream.Write(buf)
+	return conn, nil
+}
+
+func init() {
+	RunningClient = make(map[net.Addr]hyClient.Client)
+	common.Must(internet.RegisterTransportDialer(protocolName, Dial))
+}

transport/internet/hysteria2/hub.go
@@ -1,0 +1,128 @@
+package hysteria2
+
+import (
+	"context"
+
+	hyServer "github.com/apernet/hysteria/core/v2/server"
+	"github.com/apernet/quic-go"
+	"github.com/apernet/quic-go/http3"
+
+	"github.com/v2fly/v2ray-core/v5/common"
+	"github.com/v2fly/v2ray-core/v5/common/net"
+	"github.com/v2fly/v2ray-core/v5/transport/internet"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/tls"
+)
+
+// Listener is an internet.Listener that listens for TCP connections.
+type Listener struct {
+	hyServer hyServer.Server
+	rawConn  net.PacketConn
+	addConn  internet.ConnHandler
+}
+
+// Addr implements internet.Listener.Addr.
+func (l *Listener) Addr() net.Addr {
+	return l.rawConn.LocalAddr()
+}
+
+// Close implements internet.Listener.Close.
+func (l *Listener) Close() error {
+	return l.hyServer.Close()
+}
+
+func (l *Listener) StreamHijacker(ft http3.FrameType, conn quic.Connection, stream quic.Stream, err error) (bool, error) {
+	// err always == nil
+
+	tcpConn := &HyConn{
+		stream: stream,
+		local:  conn.LocalAddr(),
+		remote: conn.RemoteAddr(),
+	}
+	l.addConn(tcpConn)
+	return true, nil
+}
+
+func (l *Listener) UdpHijacker(entry *hyServer.UdpSessionEntry, originalAddr string) {
+	addr, err := net.ResolveUDPAddr("udp", originalAddr)
+	if err != nil {
+		return
+	}
+	udpConn := &HyConn{
+		IsUDPExtension:   true,
+		IsServer:         true,
+		ServerUDPSession: entry,
+		remote:           addr,
+		local:            l.rawConn.LocalAddr(),
+	}
+	l.addConn(udpConn)
+}
+
+// Listen creates a new Listener based on configurations.
+func Listen(ctx context.Context, address net.Address, port net.Port, streamSettings *internet.MemoryStreamConfig, handler internet.ConnHandler) (internet.Listener, error) {
+	tlsConfig, err := GetServerTLSConfig(streamSettings)
+	if err != nil {
+		return nil, err
+	}
+
+	if address.Family().IsDomain() {
+		return nil, nil
+	}
+
+	config := streamSettings.ProtocolSettings.(*Config)
+	rawConn, err := internet.ListenSystemPacket(context.Background(),
+		&net.UDPAddr{
+			IP:   address.IP(),
+			Port: int(port),
+		}, streamSettings.SocketSettings)
+	if err != nil {
+		return nil, err
+	}
+
+	listener := &Listener{
+		rawConn: rawConn,
+		addConn: handler,
+	}
+
+	hyServer, err := hyServer.NewServer(&hyServer.Config{
+		Conn:                  rawConn,
+		TLSConfig:             *tlsConfig,
+		DisableUDP:            !config.GetUseUdpExtension(),
+		Authenticator:         &Authenticator{Password: config.GetPassword()},
+		StreamHijacker:        listener.StreamHijacker, // acceptStreams
+		BandwidthConfig:       hyServer.BandwidthConfig{MaxTx: config.Congestion.GetUpMbps() * MBps, MaxRx: config.GetCongestion().GetDownMbps() * MBps},
+		UdpSessionHijacker:    listener.UdpHijacker, // acceptUDPSession
+		IgnoreClientBandwidth: config.GetIgnoreClientBandwidth(),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	listener.hyServer = hyServer
+	go hyServer.Serve()
+	return listener, nil
+}
+
+func GetServerTLSConfig(streamSettings *internet.MemoryStreamConfig) (*hyServer.TLSConfig, error) {
+	config := tls.ConfigFromStreamSettings(streamSettings)
+	if config == nil {
+		return nil, newError(Hy2MustNeedTLS)
+	}
+	tlsConfig := config.GetTLSConfig()
+
+	return &hyServer.TLSConfig{Certificates: tlsConfig.Certificates, GetCertificate: tlsConfig.GetCertificate}, nil
+}
+
+type Authenticator struct {
+	Password string
+}
+
+func (a *Authenticator) Authenticate(addr net.Addr, auth string, tx uint64) (ok bool, id string) {
+	if auth == a.Password || a.Password == "" {
+		return true, "user"
+	}
+	return false, ""
+}
+
+func init() {
+	common.Must(internet.RegisterTransportListener(protocolName, Listen))
+}

parhelia512/v2ray-core

main/distro/all/all.go
@@ -52,6 +52,7 @@ import (
 	_ "github.com/v2fly/v2ray-core/v4/proxy/vlite/inbound"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/vlite/outbound"
 
+	_ "github.com/v2fly/v2ray-core/v4/proxy/hysteria2"
 	_ "github.com/v2fly/v2ray-core/v4/proxy/shadowsocks2022"
 
 	// Transports
@@ -79,6 +80,8 @@ import (
 	_ "github.com/v2fly/v2ray-core/v4/transport/internet/dtls"
 
 	_ "github.com/v2fly/v2ray-core/v4/transport/internet/httpupgrade"
+
+	_ "github.com/v2fly/v2ray-core/v4/transport/internet/hysteria2"
 
 	// Transport headers
 	_ "github.com/v2fly/v2ray-core/v4/transport/internet/headers/http"

proxy/hysteria2/protocol.go
@@ -1,0 +1,207 @@
+//go:build !confonly
+// +build !confonly
+
+package hysteria2
+
+import (
+	"io"
+	gonet "net"
+
+	hyProtocol "github.com/apernet/hysteria/core/v2/international/protocol"
+	"github.com/apernet/quic-go/quicvarint"
+
+	"github.com/v2fly/v2ray-core/v4/common/buf"
+	"github.com/v2fly/v2ray-core/v4/common/net"
+	hyTransport "github.com/v2fly/v2ray-core/v4/transport/internet/hysteria2"
+)
+
+// ConnWriter is TCP Connection Writer Wrapper
+type ConnWriter struct {
+	io.Writer
+	Target        net.Destination
+	Account       *MemoryAccount
+	TCPHeaderSent bool
+}
+
+// Write implements io.Writer
+func (c *ConnWriter) Write(p []byte) (n int, err error) {
+	if !c.TCPHeaderSent {
+		if err := c.writeTCPHeader(); err != nil {
+			return 0, newError("failed to write request header").Base(err)
+		}
+	}
+
+	return c.Writer.Write(p)
+}
+
+// WriteMultiBuffer implements buf.Writer
+func (c *ConnWriter) WriteMultiBuffer(mb buf.MultiBuffer) error {
+	defer buf.ReleaseMulti(mb)
+
+	for _, b := range mb {
+		if !b.IsEmpty() {
+			if _, err := c.Write(b.Bytes()); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (c *ConnWriter) WriteTCPHeader() error {
+	if !c.TCPHeaderSent {
+		if err := c.writeTCPHeader(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func QuicLen(s int) int {
+	return int(quicvarint.Len(uint64(s)))
+}
+
+func (c *ConnWriter) writeTCPHeader() error {
+	c.TCPHeaderSent = true
+
+	// TODO: the padding length here should be randomized
+
+	padding := "Jimmy Was Here"
+	paddingLen := len(padding)
+	addressAndPort := c.Target.NetAddr()
+	addressLen := len(addressAndPort)
+	size := QuicLen(addressLen) + addressLen + QuicLen(paddingLen) + paddingLen
+
+	if size > hyProtocol.MaxAddressLength+hyProtocol.MaxPaddingLength {
+		return newError("invalid header length")
+	}
+
+	buf := make([]byte, size)
+	i := hyProtocol.VarintPut(buf, uint64(addressLen))
+	i += copy(buf[i:], addressAndPort)
+	i += hyProtocol.VarintPut(buf[i:], uint64(paddingLen))
+	copy(buf[i:], padding)
+
+	_, err := c.Writer.Write(buf)
+	return err
+}
+
+// PacketWriter UDP Connection Writer Wrapper
+type PacketWriter struct {
+	io.Writer
+	HyConn *hyTransport.HyConn
+	Target net.Destination
+}
+
+// WriteMultiBuffer implements buf.Writer
+func (w *PacketWriter) WriteMultiBuffer(mb buf.MultiBuffer) error {
+	for _, b := range mb {
+		if b.IsEmpty() {
+			continue
+		}
+		if _, err := w.writePacket(b.Bytes(), w.Target); err != nil {
+			buf.ReleaseMulti(mb)
+			return err
+		}
+	}
+
+	return nil
+}
+
+// WriteMultiBufferWithMetadata writes udp packet with destination specified
+func (w *PacketWriter) WriteMultiBufferWithMetadata(mb buf.MultiBuffer, dest net.Destination) error {
+	for _, b := range mb {
+		if b.IsEmpty() {
+			continue
+		}
+		if _, err := w.writePacket(b.Bytes(), dest); err != nil {
+			buf.ReleaseMulti(mb)
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (w *PacketWriter) WriteTo(payload []byte, addr gonet.Addr) (int, error) {
+	dest := net.DestinationFromAddr(addr)
+
+	return w.writePacket(payload, dest)
+}
+
+func (w *PacketWriter) writePacket(payload []byte, dest net.Destination) (int, error) {
+	return w.HyConn.WritePacket(payload, dest)
+}
+
+// ConnReader is TCP Connection Reader Wrapper
+type ConnReader struct {
+	io.Reader
+	Target net.Destination
+}
+
+// Read implements io.Reader
+func (c *ConnReader) Read(p []byte) (int, error) {
+	return c.Reader.Read(p)
+}
+
+// ReadMultiBuffer implements buf.Reader
+func (c *ConnReader) ReadMultiBuffer() (buf.MultiBuffer, error) {
+	b := buf.New()
+	_, err := b.ReadFrom(c)
+	return buf.MultiBuffer{b}, err
+}
+
+// PacketPayload combines udp payload and destination
+type PacketPayload struct {
+	Target net.Destination
+	Buffer buf.MultiBuffer
+}
+
+// PacketReader is UDP Connection Reader Wrapper
+type PacketReader struct {
+	io.Reader
+	HyConn *hyTransport.HyConn
+}
+
+// ReadMultiBuffer implements buf.Reader
+func (r *PacketReader) ReadMultiBuffer() (buf.MultiBuffer, error) {
+	p, err := r.ReadMultiBufferWithMetadata()
+	if p != nil {
+		return p.Buffer, err
+	}
+	return nil, err
+}
+
+// ReadMultiBufferWithMetadata reads udp packet with destination
+func (r *PacketReader) ReadMultiBufferWithMetadata() (*PacketPayload, error) {
+	_, data, dest, err := r.HyConn.ReadPacket()
+	if err != nil {
+		return nil, err
+	}
+	b := buf.FromBytes(data)
+	return &PacketPayload{Target: *dest, Buffer: buf.MultiBuffer{b}}, nil
+}
+
+type PacketConnectionReader struct {
+	reader  *PacketReader
+	payload *PacketPayload
+}
+
+func (r *PacketConnectionReader) ReadFrom(p []byte) (n int, addr gonet.Addr, err error) {
+	if r.payload == nil || r.payload.Buffer.IsEmpty() {
+		r.payload, err = r.reader.ReadMultiBufferWithMetadata()
+		if err != nil {
+			return
+		}
+	}
+
+	addr = &gonet.UDPAddr{
+		IP:   r.payload.Target.Address.IP(),
+		Port: int(r.payload.Target.Port),
+	}
+
+	r.payload.Buffer, n = buf.SplitFirstBytes(r.payload.Buffer, p)
+
+	return
+}

transport/internet/hysteria2/dialer.go
@@ -1,0 +1,202 @@
+//go:build !confonly
+// +build !confonly
+
+package hysteria2
+
+import (
+	"context"
+	"sync"
+
+	hyClient "github.com/apernet/hysteria/core/v2/client"
+	hyProtocol "github.com/apernet/hysteria/core/v2/international/protocol"
+	"github.com/apernet/quic-go/quicvarint"
+
+	"github.com/v2fly/v2ray-core/v4/common"
+	"github.com/v2fly/v2ray-core/v4/common/net"
+	"github.com/v2fly/v2ray-core/v4/common/session"
+	"github.com/v2fly/v2ray-core/v4/transport/internet"
+	"github.com/v2fly/v2ray-core/v4/transport/internet/tls"
+)
+
+var RunningClient map[net.Addr](hyClient.Client)
+var ClientMutex sync.Mutex
+var MBps uint64 = 1000000 / 8 // MByte
+
+func GetClientTLSConfig(streamSettings *internet.MemoryStreamConfig) (*hyClient.TLSConfig, error) {
+	config := tls.ConfigFromStreamSettings(streamSettings)
+	if config == nil {
+		return nil, newError(Hy2MustNeedTLS)
+	}
+	tlsConfig := config.GetTLSConfig()
+
+	return &hyClient.TLSConfig{
+		RootCAs:               tlsConfig.RootCAs,
+		ServerName:            tlsConfig.ServerName,
+		InsecureSkipVerify:    tlsConfig.InsecureSkipVerify,
+		VerifyPeerCertificate: tlsConfig.VerifyPeerCertificate,
+	}, nil
+}
+
+func ResolveAddress(dest net.Destination) (net.Addr, error) {
+	var destAddr *net.UDPAddr
+	if dest.Address.Family().IsIP() {
+		destAddr = &net.UDPAddr{
+			IP:   dest.Address.IP(),
+			Port: int(dest.Port),
+		}
+	} else {
+		addr, err := net.ResolveUDPAddr("udp", dest.NetAddr())
+		if err != nil {
+			return nil, err
+		}
+		destAddr = addr
+	}
+	return destAddr, nil
+}
+
+type connFactory struct {
+	hyClient.ConnFactory
+
+	NewFunc func(addr net.Addr) (net.PacketConn, error)
+}
+
+func (f *connFactory) New(addr net.Addr) (net.PacketConn, error) {
+	return f.NewFunc(addr)
+}
+
+func NewHyClient(serverAddr net.Addr, streamSettings *internet.MemoryStreamConfig) (hyClient.Client, error) {
+	tlsConfig, err := GetClientTLSConfig(streamSettings)
+	if err != nil {
+		return nil, err
+	}
+
+	config := streamSettings.ProtocolSettings.(*Config)
+	client, _, err := hyClient.NewClient(&hyClient.Config{
+		Auth:       config.GetPassword(),
+		TLSConfig:  *tlsConfig,
+		ServerAddr: serverAddr,
+		ConnFactory: &connFactory{
+			NewFunc: func(addr net.Addr) (net.PacketConn, error) {
+				rawConn, err := internet.ListenSystemPacket(context.Background(), &net.UDPAddr{
+					IP:   []byte{0, 0, 0, 0},
+					Port: 0,
+				}, streamSettings.SocketSettings)
+				if err != nil {
+					return nil, err
+				}
+				return rawConn.(*net.UDPConn), nil
+			},
+		},
+		BandwidthConfig: hyClient.BandwidthConfig{MaxTx: config.Congestion.GetUpMbps() * MBps, MaxRx: config.GetCongestion().GetDownMbps() * MBps},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func CloseHyClient(serverAddr net.Addr) error {
+	ClientMutex.Lock()
+	defer ClientMutex.Unlock()
+
+	client, found := RunningClient[serverAddr]
+	if found {
+		delete(RunningClient, serverAddr)
+		return client.Close()
+	}
+	return nil
+}
+
+func GetHyClient(serverAddr net.Addr, streamSettings *internet.MemoryStreamConfig) (hyClient.Client, error) {
+	var err error
+	var client hyClient.Client
+
+	ClientMutex.Lock()
+	client, found := RunningClient[serverAddr]
+	ClientMutex.Unlock()
+	if !found || !CheckHyClientHealthy(client) {
+		if found {
+			// retry
+			CloseHyClient(serverAddr)
+		}
+		client, err = NewHyClient(serverAddr, streamSettings)
+		if err != nil {
+			return nil, err
+		}
+		ClientMutex.Lock()
+		RunningClient[serverAddr] = client
+		ClientMutex.Unlock()
+	}
+	return client, nil
+}
+
+func CheckHyClientHealthy(client hyClient.Client) bool {
+	quicConn := client.GetQuicConn()
+	if quicConn == nil {
+		return false
+	}
+	select {
+	case <-quicConn.Context().Done():
+		return false
+	default:
+	}
+	return true
+}
+
+func Dial(ctx context.Context, dest net.Destination, streamSettings *internet.MemoryStreamConfig) (internet.Connection, error) {
+	config := streamSettings.ProtocolSettings.(*Config)
+
+	serverAddr, err := ResolveAddress(dest)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := GetHyClient(serverAddr, streamSettings)
+	if err != nil {
+		CloseHyClient(serverAddr)
+		return nil, err
+	}
+
+	quicConn := client.GetQuicConn()
+	conn := &HyConn{
+		local:  quicConn.LocalAddr(),
+		remote: quicConn.RemoteAddr(),
+	}
+
+	outbound := session.OutboundFromContext(ctx)
+	network := net.Network_TCP
+	if outbound != nil {
+		network = outbound.Target.Network
+		conn.Target = outbound.Target
+	}
+
+	if network == net.Network_UDP && config.GetUseUdpExtension() { // only hysteria2 can use udpExtension
+		conn.IsUDPExtension = true
+		conn.IsServer = false
+		conn.ClientUDPSession, err = client.UDP()
+		if err != nil {
+			CloseHyClient(serverAddr)
+			return nil, err
+		}
+		return conn, nil
+	}
+
+	conn.stream, err = client.OpenStream()
+	if err != nil {
+		CloseHyClient(serverAddr)
+		return nil, err
+	}
+
+	// write TCP frame type
+	frameSize := int(quicvarint.Len(hyProtocol.FrameTypeTCPRequest))
+	buf := make([]byte, frameSize)
+	hyProtocol.VarintPut(buf, hyProtocol.FrameTypeTCPRequest)
+	conn.stream.Write(buf)
+	return conn, nil
+}
+
+func init() {
+	RunningClient = make(map[net.Addr]hyClient.Client)
+	common.Must(internet.RegisterTransportDialer(protocolName, Dial))
+}

transport/internet/hysteria2/hub.go
@@ -1,0 +1,131 @@
+//go:build !confonly
+// +build !confonly
+
+package hysteria2
+
+import (
+	"context"
+
+	hyServer "github.com/apernet/hysteria/core/v2/server"
+	"github.com/apernet/quic-go"
+	"github.com/apernet/quic-go/http3"
+
+	"github.com/v2fly/v2ray-core/v4/common"
+	"github.com/v2fly/v2ray-core/v4/common/net"
+	"github.com/v2fly/v2ray-core/v4/transport/internet"
+	"github.com/v2fly/v2ray-core/v4/transport/internet/tls"
+)
+
+// Listener is an internet.Listener that listens for TCP connections.
+type Listener struct {
+	hyServer hyServer.Server
+	rawConn  net.PacketConn
+	addConn  internet.ConnHandler
+}
+
+// Addr implements internet.Listener.Addr.
+func (l *Listener) Addr() net.Addr {
+	return l.rawConn.LocalAddr()
+}
+
+// Close implements internet.Listener.Close.
+func (l *Listener) Close() error {
+	return l.hyServer.Close()
+}
+
+func (l *Listener) StreamHijacker(ft http3.FrameType, conn quic.Connection, stream quic.Stream, err error) (bool, error) {
+	// err always == nil
+
+	tcpConn := &HyConn{
+		stream: stream,
+		local:  conn.LocalAddr(),
+		remote: conn.RemoteAddr(),
+	}
+	l.addConn(tcpConn)
+	return true, nil
+}
+
+func (l *Listener) UdpHijacker(entry *hyServer.UdpSessionEntry, originalAddr string) {
+	addr, err := net.ResolveUDPAddr("udp", originalAddr)
+	if err != nil {
+		return
+	}
+	udpConn := &HyConn{
+		IsUDPExtension:   true,
+		IsServer:         true,
+		ServerUDPSession: entry,
+		remote:           addr,
+		local:            l.rawConn.LocalAddr(),
+	}
+	l.addConn(udpConn)
+}
+
+// Listen creates a new Listener based on configurations.
+func Listen(ctx context.Context, address net.Address, port net.Port, streamSettings *internet.MemoryStreamConfig, handler internet.ConnHandler) (internet.Listener, error) {
+	tlsConfig, err := GetServerTLSConfig(streamSettings)
+	if err != nil {
+		return nil, err
+	}
+
+	if address.Family().IsDomain() {
+		return nil, nil
+	}
+
+	config := streamSettings.ProtocolSettings.(*Config)
+	rawConn, err := internet.ListenSystemPacket(context.Background(),
+		&net.UDPAddr{
+			IP:   address.IP(),
+			Port: int(port),
+		}, streamSettings.SocketSettings)
+	if err != nil {
+		return nil, err
+	}
+
+	listener := &Listener{
+		rawConn: rawConn,
+		addConn: handler,
+	}
+
+	hyServer, err := hyServer.NewServer(&hyServer.Config{
+		Conn:                  rawConn,
+		TLSConfig:             *tlsConfig,
+		DisableUDP:            !config.GetUseUdpExtension(),
+		Authenticator:         &Authenticator{Password: config.GetPassword()},
+		StreamHijacker:        listener.StreamHijacker, // acceptStreams
+		BandwidthConfig:       hyServer.BandwidthConfig{MaxTx: config.Congestion.GetUpMbps() * MBps, MaxRx: config.GetCongestion().GetDownMbps() * MBps},
+		UdpSessionHijacker:    listener.UdpHijacker, // acceptUDPSession
+		IgnoreClientBandwidth: config.GetIgnoreClientBandwidth(),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	listener.hyServer = hyServer
+	go hyServer.Serve()
+	return listener, nil
+}
+
+func GetServerTLSConfig(streamSettings *internet.MemoryStreamConfig) (*hyServer.TLSConfig, error) {
+	config := tls.ConfigFromStreamSettings(streamSettings)
+	if config == nil {
+		return nil, newError(Hy2MustNeedTLS)
+	}
+	tlsConfig := config.GetTLSConfig()
+
+	return &hyServer.TLSConfig{Certificates: tlsConfig.Certificates, GetCertificate: tlsConfig.GetCertificate}, nil
+}
+
+type Authenticator struct {
+	Password string
+}
+
+func (a *Authenticator) Authenticate(addr net.Addr, auth string, tx uint64) (ok bool, id string) {
+	if auth == a.Password || a.Password == "" {
+		return true, "user"
+	}
+	return false, ""
+}
+
+func init() {
+	common.Must(internet.RegisterTransportListener(protocolName, Listen))
+}